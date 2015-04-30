19169a7928a5f31c1ea3851f114de5525e9def89
William Han <whan@godaddy.com>
2015-04-29 22:48:53 -0700

    add TestNG example

--- essential/lambda/Lambda.java
+++ essential/lambda/Lambda.java
@@ -1,7 +1,16 @@
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 public class Lambda {
 
     public static void main(String[] args) {
-        Runnable r = () -> System.out.println("Hello World!");
+        Runnable r = () -> System.out.println("Hello World!");  // void run();
         r.run();
+
+        Predicate<String> predicate = (s) -> s == "true";  // boolean test(T t);
+        System.out.println(predicate.test("true"));  // true
+
+        Function<String, Integer> function = s -> s.length();
+        System.out.println(function.apply("Hello"));  // 5
     }
 }
---
+++ test/TestNG/SimpleTestNGProject/src/NewClassForTesting.java
@@ -0,0 +1,19 @@
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+@Test ()
+public class NewClassForTesting {
+    @DataProvider
+    public Object[][] data() {
+        return new String[][]{
+                new String[]{"data1"}, new String[]{"data2"}
+        };
+    }
+
+    @Test(dataProvider = "data")
+    public void test(String d) {
+        System.out.println(d);
+        Assert.assertEquals("First", "First");
+    }
+}
