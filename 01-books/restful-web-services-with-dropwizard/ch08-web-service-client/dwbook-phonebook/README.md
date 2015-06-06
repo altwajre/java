fedea28b9a5fe0687e9319ac1cda50c423961dad
William Han <whan@godaddy.com>
2015-06-06 16:47:08 -0700

    add web service client

---
+++ 01-books/restful-web-services-with-dropwizard/ch08-web-service-client/dwbook-phonebook/README.md
@@ -0,0 +1,54 @@
+# web-service-client - dwbook-phonebook
+
+maven-archetype-quickstart project
+
+## Start mysql server
+
+> `mysql.server start`
+
+> `mysql.server stop` when you are done
+
+## Compile
+
+> `mvn package` or `mvn clean install`
+
+## Run the application
+
+> `java -jar target/dwbook-phonebook-1.0-SNAPSHOT.jar server config.yml`
+
+## Visit
+
+> Open chrome, developer tool, Network
+
+### POST
+
+> http://localhost:8080/client/newContact?firstName=Jane&lastName=Doe&phone=98765432
+
+```
+Remote Address:[::1]:8080
+Request URL:http://localhost:8080/client/newContact?firstName=Jane&lastName=Doe&phone=98765432
+Request Method:GET
+Status Code:201 Created
+```
+
+### PUT
+
+> http://localhost:8080/client/updateContact?id=3&firstName=Alex&lastName=Updated&phone=3210465
+
+{
+id: 3,
+firstName: "Alex",
+lastName: "Updated",
+phone: "3210465"
+}
+
+### DELETE
+
+> http://localhost:8080/client/deleteContact?id=3
+
+```
+Remote Address:[::1]:8080
+Request URL:http://localhost:8080/client/deleteContact?id=8
+Request Method:GET
+Status Code:204 No Content
+```
---
+++ 01-books/restful-web-services-with-dropwizard/ch08-web-service-client/dwbook-phonebook/src/main/java/com/dwbook/phonebook/resources/ContactResource.java
@@ -0,0 +1,75 @@
+package com.dwbook.phonebook.resources;
+
+import com.dwbook.phonebook.dao.ContactDAO;
+import com.dwbook.phonebook.representations.Contact;
+import org.skife.jdbi.v2.DBI;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Valid;
+import javax.validation.Validator;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
+
+@Path("/contact")
+@Produces(MediaType.APPLICATION_JSON)
+public class ContactResource {
+
+    private final ContactDAO contactDao;
+
+    public ContactResource(DBI jdbi) {
+        contactDao = jdbi.onDemand(ContactDAO.class);
+    }
+
+    @GET
+    @Path("/all")
+    public Response getContacts() {
+        // retrieve information about the contact with the provided id
+        Iterator<Contact> contacts = contactDao.getContacts();
+        return Response.ok(contacts).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response getContact(@PathParam("id") int id) {
+        // retrieve information about the contact with the provided id
+        Contact contact = contactDao.getContactById(id);
+        return Response.ok(contact).build();
+    }
+
+    @POST
+    public Response createContact(@Valid Contact contact) throws URISyntaxException {
+        // OK, no validation errors
+        // Store the new contact
+        int newContactId = contactDao.createContact(contact.getFirstName(),
+                contact.getLastName(), contact.getPhone());
+        return Response.created(new URI(String.valueOf(newContactId))).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Response updateContact(
+            @PathParam("id") int id,
+            @Valid Contact contact) {
+        contactDao.updateContact(id, contact.getFirstName(),
+                contact.getLastName(), contact.getPhone());
+        return Response.ok(
+                new Contact(id, contact.getFirstName(), contact.getLastName(),
+                        contact.getPhone())).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response deleteContact(@PathParam("id") int id) {
+        // delete the contact with the provided id
+        // ...
+        contactDao.deleteContact(id);
+        return Response.noContent().build();
+    }
+}
