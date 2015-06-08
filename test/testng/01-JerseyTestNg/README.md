1f5016e522d9194e0f13cce3cb28c1195c419c54
William Han <whan@godaddy.com>
2015-06-08 01:09:25 -0700

    add JerseyTestNg example

---
+++ test/testng/01-JerseyTestNg/README.md
@@ -0,0 +1,9 @@
+# JerseyTestNg
+
+https://github.com/jersey/jersey/tree/master/tests/e2e-testng/src/test/java/org/glassfish/jersey/tests/e2e
+
+Compile in intellij
+
+## Run tests
+
+> In intellij, right click on test and select run test
---
+++ test/testng/01-JerseyTestNg/src/test/java/com/dwbook/tests/ContainerPerClassTest.java
@@ -0,0 +1,58 @@
+package com.dwbook.tests;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+
+import javax.inject.Singleton;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTestNg;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+
+public class ContainerPerClassTest extends JerseyTestNg.ContainerPerClassTest{
+
+    @Path("/")
+    @Singleton
+    @Produces("text/plain")
+    public static class Resource {
+
+        private int i = 1;
+
+        @GET
+        public int get() {
+            return i++;
+        }
+    }
+
+    @Override
+    protected Application configure() {
+        return new ResourceConfig(Resource.class);
+    }
+
+    @Test(priority = 1)
+    public void first() throws Exception {
+        test(1);
+    }
+
+    @Test(priority = 2)
+    public void second() throws Exception {
+        test(2);
+    }
+
+    @Test(priority = 3)
+    public void third() throws Exception {
+        test(3);
+    }
+
+    private void test(final Integer expected) {
+        final Response response = target().request().get();
+
+        assertEquals(response.getStatus(), 200);
+        assertEquals(response.readEntity(Integer.class), expected);
+    }
+}
