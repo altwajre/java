b1f25a5e27b70468b8cd4841eb61a6bf9fdec451
William Han <whan@godaddy.com>
2015-06-10 23:12:55 -0700

    add slf4j example

---
+++ maven/02-mvnrepository/log/slf4j/README.md
@@ -0,0 +1,14 @@
+# slf4j
+
+> http://www.slf4j.org/docs.html
+
+> https://github.com/qos-ch/slf4j
+
+> http://runjva.appspot.com/logging101/index.html - example
+
+## Run Application
+
+### Intellij
+
+> Run, Run 'App'
+
---
+++ maven/02-mvnrepository/log/slf4j/src/main/java/com/company/test/App.java
@@ -0,0 +1,12 @@
+package com.company.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App
+{
+    public static void main(String[] args) throws Exception {
+        Logger log = LoggerFactory.getLogger(App.class);
+        log.info("Hello World");
+    }
+}
