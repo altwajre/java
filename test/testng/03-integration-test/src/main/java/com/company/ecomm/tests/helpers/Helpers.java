858608744aaf44c627db2e476a7e0bfa1d158720
William Han <whan@godaddy.com>
2015-06-11 09:54:12 -0700

    fix build

--- test/testng/03-integration-test/src/main/java/com/company/ecomm/tests/helpers/Helpers.java
+++ test/testng/03-integration-test/src/main/java/com/company/ecomm/tests/helpers/Helpers.java
@@ -2,17 +2,18 @@ package com.company.ecomm.tests.helpers;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import com.sun.istack.internal.logging.Logger;
 import org.apache.http.util.EntityUtils;
 import org.json.simple.JSONObject;
 import org.mortbay.util.ajax.JSON;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.Random;
 
 public class Helpers {
     Random generator = new Random();
-    Logger logger = Logger.getLogger(Helpers.class);
+    Logger logger = LoggerFactory.getLogger(Helpers.class);
 
     public JSONObject httpToJson(HttpResponse response) throws IOException {
         return (JSONObject) JSON.parse(EntityUtils.toString(response.getEntity()));
