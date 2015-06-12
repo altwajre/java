87cc4936f0ae5e5d55dbbb8890a3bab21af4729f
William Han <whan@godaddy.com>
2015-06-12 00:07:57 -0700

    add built-in logging

---
+++ maven/02-mvnrepository/command-line-parsers/apache-commons-cli/src/main/java/com/company/test/Cli.java
@@ -0,0 +1,11 @@
+package com.company.test;
+
+import java.util.logging.Logger;
+
+public class Cli {
+    private static final Logger log = Logger.getLogger(Cli.class.getName());
+
+    public Cli(String[] args){
+        log.info("Hello World");
+    }
+}
---
+++ maven/02-mvnrepository/logging/03-java-util-logging/src/main/java/com/company/test/App.java
@@ -0,0 +1,12 @@
+package com.company.test;
+
+import java.util.logging.Logger;
+
+public class App
+{
+    static final Logger logger = Logger.getLogger(App.class.getName());
+    public static void main( String[] args )
+    {
+        logger.info("Hello World");
+    }
+}
