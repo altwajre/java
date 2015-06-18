3f87cb5837c80608d9f59395c71b7430f2e6aaf4
William Han <whan@godaddy.com>
2015-06-17 18:30:28 -0700

    delete old integration tests example

--- test/testng/031-integration-tests/src/main/java/com/company/ecomm/tests/client/HttpRequstBuilder.java
+++
@@ -1,7 +0,0 @@
-package com.company.ecomm.tests.client;
-
-/**
- * Created by whan on 6/11/15.
- */
-public class HttpRequstBuilder {
-}
--- test/testng/031-integration-tests/src/main/java/com/company/ecomm/tests/testbase/TestContext.java
+++
@@ -1,8 +0,0 @@
-package com.company.ecomm.tests.testbase;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class TestContext {
-    Logger logger = LoggerFactory.getLogger(TestContext.class);
-}
