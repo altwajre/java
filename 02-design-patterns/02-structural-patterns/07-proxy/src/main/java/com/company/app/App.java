a9187ae05fa9b8fb3d9410d4033883e25d4c7dc4
William Han <whan@godaddy.com>
2015-06-21 23:03:30 -0700

    add proxy example

---
+++ 02-design-patterns/02-structural-patterns/07-proxy/src/main/java/com/company/app/App.java
@@ -0,0 +1,32 @@
+package com.company.app;
+
+public class App
+{
+    public interface IMath{  // Subject interface
+        double add(double x, double y);
+        double sub(double x, double y);
+        double mul(double x, double y);
+        double div(double x, double y);
+    }
+    static class Math implements IMath{  // Real Subject
+        public double add(double x, double y) { return x + y; }
+        public double sub(double x, double y) { return x - y; }
+        public double mul(double x, double y) { return x * y; }
+        public double div(double x, double y) { return x / y; }
+    }
+    static class MathProxy implements IMath{
+        private Math math = new Math();
+        public double add(double x, double y) { return math.add(x, y); }
+        public double sub(double x, double y) { return math.sub(x, y); }
+        public double mul(double x, double y) { return math.mul(x, y); }
+        public double div(double x, double y) { return math.div(x, y); }
+    }
+    public static void main( String[] args )
+    {
+        MathProxy proxy = new MathProxy();
+        System.out.format("4 + 2 = %s\n", proxy.add(4, 2));
+        System.out.format("4 - 2 = %s\n", proxy.sub(4, 2));
+        System.out.format("4 * 2 = %s\n", proxy.mul(4, 2));
+        System.out.format("4 / 2 = %s\n", proxy.div(4, 2));
+    }
+}
