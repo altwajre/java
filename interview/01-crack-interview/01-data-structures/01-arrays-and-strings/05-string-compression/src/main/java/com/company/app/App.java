4607b6dd34198656b58e8ea5092c96207c931236
William Han <whan@godaddy.com>
2015-07-04 11:32:41 -0700

    add string compression example

---
+++ interview/01-crack-interview/01-data-structures/01-arrays-and-strings/05-string-compression/src/main/java/com/company/app/App.java
@@ -0,0 +1,67 @@
+package com.company.app;
+
+/*
+Q: basic string compression using the counts of repeated chars.
+For example, the string aabcccccaaa would become a2blc5a3.
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        countCompressionTest();
+        compressTest();
+    }
+
+    private static void countCompressionTest() {
+        System.out.println("abc".charAt(0));  // a
+        System.out.println(countCompression("aaaaa"));  // 2
+        System.out.println(countCompression("abc"));  // 6
+        System.out.println(countCompression("aabcccccaaa"));  // 8
+    }
+    static int countCompression(String str){
+        if(str == null || str.isEmpty()) return 0;
+        char current = str.charAt(0);
+        int size = 0;
+        int count = 1;
+        for(int i = 1; i < str.length(); i++){
+            if(str.charAt(i) == current){  // found repeated char
+                count++;
+            }
+            else{
+                size += 1 + String.valueOf(count).length();  // increase size: size = size + 1 + countLength
+                current = str.charAt(i);  // store new char to current
+                count = 1;
+            }
+        }
+        size += 1 + String.valueOf(count).length();  // increase size: size = size + 1 + countLength
+        return size;
+    }
+
+    private static void compressTest() {
+        System.out.println(compress("aabcccccaaa"));  // a2b1c5a3
+    }
+    static String compress(String str){
+        // check if compression would create a longer string
+        int size = countCompression(str);  // get compression size
+        if(size >= str.length()){
+            return str;
+        }
+        StringBuffer strBuf = new StringBuffer();
+        char current = str.charAt(0);
+        int count = 1;
+        for(int i = 1; i < str.length(); i++){
+            if(str.charAt(i) == current){  // found repeated char
+                count++;
+            }
+            else{  // insert char count, and update last char
+                strBuf.append(current);  // insert char
+                strBuf.append(count);  // insert count
+                current = str.charAt(i);
+                count = 1;
+            }
+        }
+        strBuf.append(current);
+        strBuf.append(count);
+        return strBuf.toString();
+    }
+}
