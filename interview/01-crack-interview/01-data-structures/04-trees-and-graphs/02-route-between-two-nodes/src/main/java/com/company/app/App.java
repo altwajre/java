8eda0a314fe3fd6a31fbacd4ffed73b59dbadeb2
William Han <whan@godaddy.com>
2015-07-17 23:15:04 -0700

    update reachable nodes example

--- interview/01-crack-interview/01-data-structures/04-trees-and-graphs/02-route-between-two-nodes/src/main/java/com/company/app/App.java
+++ interview/01-crack-interview/01-data-structures/04-trees-and-graphs/02-route-between-two-nodes/src/main/java/com/company/app/App.java
@@ -2,87 +2,57 @@ package com.company.app;
 
 import java.util.*;
 
-/*
-Q: Given a directed graph, find out whether there is a route between two nodes.
-
-A: breath first search
- */
 public class App
 {
-    static class Node {
-        private int value;
-        private boolean discovered;
-        private List<Node> adj;
-        public Node(int value) {
+    static class Node{
+        public int value;
+        public boolean isVisited;
+        public List<Node> adjacent;
+        public Node(int value){
             this.value = value;
-            this.adj = new ArrayList<Node>();
+            this.adjacent = new ArrayList<Node>();
         }
-        public int getValue() { return value; }
-        public void setValue(int value) { this.value = value; }
-        public boolean isDiscovered() { return discovered; }
-        public void setDiscovered(boolean discovered) { this.discovered = discovered; }
-        public List<Node> getAdj() { return adj; }
-        public void setAdj(List<Node> adj) { this.adj = adj; }
-        public void connect(Node node){ this.adj.add(node); }
-        @Override
-        public String toString(){ return value + ""; }
-        @Override
-        public boolean equals(Object o){
-            if(this == o){return true;}
-            if(o == null || this.getClass() != o.getClass()){ return false; }
-            Node node = (Node) o;
-            if(value != node.value){ return false; }
-            return true;
-        }
-        @Override
-        public int hashCode(){ return value; }
+        public void connect(Node node){ this.adjacent.add(node); }
     }
     static class Graph{
-        private Map<Integer, Node> nodes;
-        public Graph(int[][] adj_matrix){
-            if(adj_matrix == null || adj_matrix.length == 0){
-                throw new IllegalArgumentException("adj matrix is null or empty");
-            }
+        public Map<Integer, Node> nodes;
+        public Graph(int[][] adjacentMatrix){
             nodes = new HashMap<Integer, Node>();
             // create a graph with the an adjacency list
-            Node node1 = null;
-            Node node2 = null;
-            for(int i = 0; i < adj_matrix.length; i++){
+            Node node1, node2;
+            for(int i = 0; i < adjacentMatrix.length; i++){ // row by row - y coordinate
                 int key1 = i + 1;
                 node1 = nodes.get(key1);
                 if(node1 == null){
                     node1 = new Node(key1);
                     nodes.put(key1, node1);
                 }
-                for(int j = 0; j < adj_matrix[i].length; j++){
+                for(int j = 0; j < adjacentMatrix[i].length; j++){  // x coordinate
                     int key2 = j + 1;
                     node2 = nodes.get(key2);
-                    if(node2 == null){
+                    if(node2 == null) {
                         node2 = new Node(key2);
                         nodes.put(key2, node2);
                     }
-                    if(adj_matrix[i][j] == 1){ node1.connect(node2); }
+                    if(adjacentMatrix[i][j] == 1){node1.connect(node2);}
                 }
             }
         }
-        public Map<Integer, Node> getNodes(){ return nodes; }
     }
     // perform a depth first or breath first search and see if you hit a node - O([V| + |E|)
     static boolean reachable(Graph graph, Node src, Node dest){
-        if(src.equals(dest)){ return true; }
-        // conduct a BFS or DFS and check to see if dest is reachable from src. here we do a BFS
-        for(Node node : graph.getNodes().values()){ node.setDiscovered(false); }
+        if(src == dest) {return true;}
+        for(Node node : graph.nodes.values()){node.isVisited = false;}
         Queue<Node> queue = new LinkedList<Node>();
-        src.setDiscovered(true);
+        src.isVisited = true;
         queue.add(src);
-        Node node;
         while(!queue.isEmpty()){
-            node = queue.remove();
-            for(Node adj : node.getAdj()){
-                if(adj.equals(dest)){ return true; }
-                if(!adj.isDiscovered()){
-                    adj.setDiscovered(true);
-                    queue.add(adj);
+            Node tmp = queue.remove();
+            for(Node node : tmp.adjacent){  // go through all neighbor nodes
+                if(node == dest) return true;  // reach to the dest node, return true
+                if(!node.isVisited){  // add unvisited neighbor nodes to the queue
+                    node.isVisited = true;
+                    queue.add(node);
                 }
             }
         }
@@ -91,7 +61,7 @@ public class App
     public static void main( String[] args )
     {
         // create a graph
-        /* Graph.nodes {{1,[1]},{2,[2]},{3,[3]},{4,[4]},{5,{[5]},{6,[6]}}
+        /* Graph.nodes: {{1,[1]},{2,[2]},{3,[3]},{4,[4]},{5,{[5]},{6,[6]}}
           [1]-[2]
             \ |
              \|
@@ -99,7 +69,7 @@ public class App
           |
          [6]
          */
-        int[][] adjacency_matrix = {
+        int[][] adjacencyMatrix = {
                 // 1  2  3  4  5  6
          /* 1 */  {1, 1, 0, 0, 1, 0},
          /* 2 */  {1, 0, 0, 0, 1, 0},
@@ -108,13 +78,13 @@ public class App
          /* 5 */  {1, 1, 0, 1, 0, 0},
          /* 6 */  {0, 0, 0, 1, 0, 0}
         };
-        Graph graph = new Graph(adjacency_matrix);
-        Map<Integer, Node> nodes = graph.getNodes();
-        Node src = nodes.get(2);
-        Node dest = nodes.get(3);
+        Graph graph = new Graph(adjacencyMatrix);
+        Map<Integer, Node> nodeMap = graph.nodes;
+        Node src = nodeMap.get(2);
+        Node dest = nodeMap.get(3);
         System.out.println("is 2 -> 3 reachable: " + reachable(graph, src, dest));
-        src = nodes.get(1);
-        dest = nodes.get(6);
+        src = nodeMap.get(1);
+        dest = nodeMap.get(6);
         System.out.println("is 1 -> 6 reachable: " + reachable(graph, src, dest));
     }
 }
