10aa57db0c7027cf52cf6774e3e284597a509fc4
William Han <whan@godaddy.com>
2015-07-20 16:09:47 -0700

    add create level linkedlist example

--- interview/01-crack-interview/01-data-structures/04-trees-and-graphs/03-create-minimal-bst/src/main/java/com/company/app/App.java
+++ interview/01-crack-interview/01-data-structures/04-trees-and-graphs/03-create-minimal-bst/src/main/java/com/company/app/App.java
@@ -16,12 +16,12 @@ public class App
         public Node left;
         public Node right;
     }
-    static Node createMinimalBST(int arr[], int start, int end){
-        if(end < start) return null;
-        int mid = (start + end) / 2;
+    static Node createMinimalBST(int arr[], int startIndex, int endIndex){
+        if(endIndex < startIndex) return null;
+        int mid = (startIndex + endIndex) / 2;
         Node node = new Node(arr[mid]);
-        node.left = createMinimalBST(arr, start, mid - 1);
-        node.right = createMinimalBST(arr, mid + 1, end);
+        node.left = createMinimalBST(arr, startIndex, mid - 1);
+        node.right = createMinimalBST(arr, mid + 1, endIndex);
         return node;
     }
     static void bfs(Node node){
@@ -34,6 +34,20 @@ public class App
             if(tmp.right != null) queue.add(tmp.right);
         }
     }
+    /*
+          [0,1,2,3,4,5,6,7,8] <- index
+          {1,2,3,4,5,6,7,8,9} <- data
+                   5(arr[4])
+                /          \
+          {1,2,3,4}      {6,7,8,9}
+             2(arr[1])      7
+           /   \          /   \
+         {1}  {3,4}     {6}   {8,9}
+          1    3         6     8
+                \               \
+                {4}             {9}
+                 4               9
+     */
     public static void main( String[] args )
     {
         int[] numbers = {1,2,3,4,5,6,7,8,9};
---
+++ interview/01-crack-interview/01-data-structures/04-trees-and-graphs/04-create-level-linkedlist/README.md
@@ -0,0 +1,24 @@
+# create-level-linkedlist
+
+maven-archetype-quickstart project
+
+## Build package
+
+> `mvn package` or `mvn clean install`
+
+## Run the application
+
+> `java -cp target/app-1.0-SNAPSHOT.jar com.company.app.App`
+
+```
+#createLevelLinkedListRecurseTest
+5(o:1,l:0) 
+2(o:2,l:1) 7(o:6,l:1) 
+1(o:3,l:2) 3(o:4,l:2) 6(o:7,l:2) 8(o:8,l:2) 
+4(o:5,l:3) 9(o:9,l:3) 
+#createLevelLinkedListTest
+5(1) 
+2(2) 7(3) 
+1(4) 3(5) 6(6) 8(7) 
+4(8) 9(9)
+```
