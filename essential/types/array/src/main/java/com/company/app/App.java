32b0b50156d4d052afb711137937b957063e52fb
William Han <whan@godaddy.com>
2015-08-01 10:47:46 -0700

    add find item in array example

---
+++ essential/types/array/src/main/java/com/company/app/App.java
@@ -0,0 +1,14 @@
+package com.company.app;
+
+/*
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int[] numbers = {0, 11, 22, 33, 44, 55, 66, 77, 88, 99};
+        for(int n : numbers){
+            System.out.print(n + " ");
+        }
+    }
+}
