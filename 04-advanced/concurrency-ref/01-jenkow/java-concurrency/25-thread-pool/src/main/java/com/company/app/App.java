ba61554b160d81826798bcbe38819caebf95c23b
William Han <whan@godaddy.com>
2015-09-07 10:58:55 -0700

    update thread-pool example

--- 04-advanced/concurrency-ref/01-jenkow/java-concurrency/25-thread-pool/src/main/java/com/company/app/App.java
+++ 04-advanced/concurrency-ref/01-jenkow/java-concurrency/25-thread-pool/src/main/java/com/company/app/App.java
@@ -9,50 +9,64 @@ import java.util.concurrent.TimeUnit;
 http://tutorials.jenkov.com/java-concurrency/thread-pools.html
 
 output:
-    Thread-0: PoolThread.run()
-    Thread-2: PoolThread.run()
-    Thread-1: PoolThread.run()
-done
-  Thread-0: dequeue() wait(); size=0
-  Thread-1: dequeue() wait(); size=0
-  Thread-2: dequeue() wait(); size=0
-  main: enqueue() notifyAll(); size=0
-  main: enqueue() queue.add(); size=0
-  main: enqueue() queue.add(); size=1
-  main: enqueue() wait(); size=2
-  Thread-2: dequeue() EXIT wait(); size=2
-  Thread-2: dequeue() notifyAll(); size=2
-  Thread-2: dequeue() queue.remove(); size=2
-  Thread-1: dequeue() EXIT wait(); size=1
-Thread-2: before sleep
-  Thread-1: dequeue() queue.remove(); size=1
-Thread-1: before sleep
-  Thread-0: dequeue() EXIT wait(); size=0
-  Thread-0: dequeue() wait(); size=0
-  main: enqueue() EXIT wait(); size=0
-  main: enqueue() notifyAll(); size=0
-  main: enqueue() queue.add(); size=0
-  main: enqueue() queue.add(); size=1
-  main: enqueue() wait(); size=2
-  Thread-0: dequeue() EXIT wait(); size=2
-  Thread-0: dequeue() notifyAll(); size=2
-  Thread-0: dequeue() queue.remove(); size=2
-Thread-0: before sleep
-  main: enqueue() EXIT wait(); size=1
-  main: enqueue() queue.add(); size=1
-Thread-0: after sleep
-Thread-1: after sleep
-Thread-2: after sleep
-  Thread-0: dequeue() notifyAll(); size=2
-  Thread-0: dequeue() queue.remove(); size=2
-Thread-0: before sleep
-  Thread-2: dequeue() queue.remove(); size=1
-Thread-2: before sleep
-  Thread-1: dequeue() wait(); size=0
-Thread-2: after sleep
-  Thread-2: dequeue() wait(); size=0
-Thread-0: after sleep
-  Thread-0: dequeue() wait(); size=0
+  Thread-0: dequeue entered; size()=0
+  Thread-0: dequeue wait(); size()=0
+  Thread-2: dequeue entered; size()=0
+  Thread-2: dequeue wait(); size()=0
+  Thread-1: dequeue entered; size()=0
+  Thread-1: dequeue wait(); size()=0
+main: enqueue entered 1922154895; size()=0
+main: enqueue notifyAll() 1922154895; size()=0
+main: enqueue added 1922154895; size()=1
+main: enqueue entered 1922154895; size()=1
+main: enqueue added 1922154895; size()=2
+main: enqueue entered 1922154895; size()=2
+main: enqueue wait() 1922154895; size()=2
+  Thread-1: dequeue EXIT wait(); size()=2
+  Thread-1: dequeue notifyAll(); size()=2
+  Thread-1: dequeue removed 1922154895; size()=1
+  Thread-2: dequeue EXIT wait(); size()=1
+    #Thread-1 finished task_1
+  Thread-2: dequeue removed 1922154895; size()=0
+    #Thread-2 finished task_2
+  Thread-0: dequeue EXIT wait(); size()=0
+  Thread-0: dequeue wait(); size()=0
+  Thread-2: dequeue entered; size()=0
+  Thread-2: dequeue wait(); size()=0
+  Thread-1: dequeue entered; size()=0
+  Thread-1: dequeue wait(); size()=0
+main: enqueue EXIT wait() 1922154895; size()=0
+main: enqueue notifyAll() 1922154895; size()=0
+main: enqueue added 1922154895; size()=1
+main: enqueue entered 1922154895; size()=1
+main: enqueue added 1922154895; size()=2
+main: enqueue entered 1922154895; size()=2
+main: enqueue wait() 1922154895; size()=2
+  Thread-1: dequeue EXIT wait(); size()=2
+  Thread-1: dequeue notifyAll(); size()=2
+  Thread-1: dequeue removed 1922154895; size()=1
+    #Thread-1 finished task_3
+  Thread-2: dequeue EXIT wait(); size()=1
+  Thread-2: dequeue removed 1922154895; size()=0
+    #Thread-2 finished task_4
+  Thread-0: dequeue EXIT wait(); size()=0
+  Thread-0: dequeue wait(); size()=0
+  Thread-2: dequeue entered; size()=0
+  Thread-2: dequeue wait(); size()=0
+  Thread-1: dequeue entered; size()=0
+  Thread-1: dequeue wait(); size()=0
+main: enqueue EXIT wait() 1922154895; size()=0
+main: enqueue notifyAll() 1922154895; size()=0
+main: enqueue added 1922154895; size()=1
+  Thread-1: dequeue EXIT wait(); size()=1
+  Thread-1: dequeue removed 1922154895; size()=0
+    #Thread-1 finished task_5
+  Thread-1: dequeue entered; size()=0
+  Thread-1: dequeue wait(); size()=0
+  Thread-2: dequeue EXIT wait(); size()=0
+  Thread-2: dequeue wait(); size()=0
+  Thread-0: dequeue EXIT wait(); size()=0
+  Thread-0: dequeue wait(); size()=0
 
  */
 public class App 
@@ -60,43 +74,42 @@ public class App
     static class BlockingQueue{
         private List queue = new LinkedList();
         private int limit = 10;
-        public BlockingQueue(int limit){
-            this.limit = limit;
-        }
-        public int size(){
-            return queue.size();
-        }
+        public BlockingQueue(int limit){ this.limit = limit; }
+        public int size(){ return queue.size(); }
         public synchronized void enqueue(Object item){
             String threadName = Thread.currentThread().getName();
-            while(this.queue.size() == this.limit){
+            System.out.println(threadName + ": enqueue entered " + item.hashCode() + "; size()=" + queue.size());
+            while(queue.size() == limit){
                 try {
-                    System.out.println("  "+threadName + ": enqueue() wait(); size=" + queue.size());
+                    System.out.println(threadName + ": enqueue wait() " + item.hashCode() + "; size()=" + queue.size());
                     wait();
-                    System.out.println("  " + threadName + ": enqueue() EXIT wait(); size=" + queue.size());
+                    System.out.println(threadName + ": enqueue EXIT wait() " + item.hashCode() + "; size()=" + queue.size());
                 } catch (InterruptedException e) { e.printStackTrace(); }
             }
-            if(this.queue.size() == 0){  // notify items that wait for enqueue
-                System.out.println("  "+threadName + ": enqueue() notifyAll(); size=" + queue.size());
+            if(queue.size() == 0){  // wake all waiting threads
+                System.out.println(threadName + ": enqueue notifyAll() " + item.hashCode() + "; size()=" + queue.size());
                 notifyAll();
             }
-            System.out.println("  "+threadName + ": enqueue() queue.add(); size=" + queue.size());
-            this.queue.add(item);
+            queue.add(item);
+            System.out.println(threadName + ": enqueue added " + item.hashCode() + "; size()=" + queue.size());
         }
         public synchronized Object dequeue(){
             String threadName = Thread.currentThread().getName();
-            while(this.queue.size() == 0){
+            System.out.println("  "+threadName + ": dequeue entered; size()=" + queue.size());
+            while(queue.size() == 0){
                 try {
-                    System.out.println("  "+threadName + ": dequeue() wait(); size=" + queue.size());
+                    System.out.println("  "+threadName + ": dequeue wait(); size()=" + queue.size());
                     wait();
-                    System.out.println("  " + threadName + ": dequeue() EXIT wait(); size=" + queue.size());
+                    System.out.println("  " + threadName + ": dequeue EXIT wait(); size()=" + queue.size());
                 } catch (InterruptedException e) { e.printStackTrace(); }
             }
-            if(this.queue.size() == this.limit){  // notify items that wait for dequeue
-                System.out.println("  "+threadName + ": dequeue() notifyAll(); size=" + queue.size());
+            if(queue.size() == limit){  // wake all waiting threads
+                System.out.println("  "+threadName + ": dequeue notifyAll(); size()=" + queue.size());
                 notifyAll();
             }
-            System.out.println("  "+threadName + ": dequeue() queue.remove(); size=" + queue.size());
-            return this.queue.remove(0);
+            Object temp = queue.remove(0);
+            System.out.println("  " + threadName + ": dequeue removed "+temp.hashCode()+"; size()=" + queue.size());
+            return temp;
         }
     }
     static class PoolThread extends Thread{
@@ -104,10 +117,9 @@ public class App
         private boolean isStopped = false;
         public PoolThread(BlockingQueue queue){ taskQueue = queue; }
         public void run(){
-            System.out.println("    "+Thread.currentThread().getName() + ": PoolThread.run()");
             while(!isStopped){
                 try{
-                    Runnable runnable = (Runnable) taskQueue.dequeue();
+                    Runnable runnable = (Runnable) taskQueue.dequeue(); // each thread is waiting for task to run
                     runnable.run();
                 } catch (Exception e){ e.printStackTrace(); }
             }
@@ -126,7 +138,6 @@ public class App
             taskQueue = new BlockingQueue(maxNoOfTasks);
             for(int i = 0; i < noOfThreads; i++){ threads.add(new PoolThread(taskQueue)); }
             for(PoolThread thread : threads){ thread.start(); }
-            System.out.println("done");
         }
         public synchronized void execute(Runnable task) throws Exception{
             if(this.isStopped){ throw new IllegalStateException("ThreadPool is stopped"); }
@@ -137,16 +148,14 @@ public class App
             for(PoolThread thread : threads){ thread.stop(); }
         }
     }
+    static int taskCount = 1;
     public static void main( String[] args ) throws Exception {
         int noIfThreads = 3;
         int maxNoOfTasks = 2;
         ThreadPool threadPool = new ThreadPool(noIfThreads, maxNoOfTasks);
+        int count = 1;
         Runnable task = () -> {
-            String threadName = Thread.currentThread().getName();
-            System.out.println(threadName + ": before sleep");
-            try { TimeUnit.SECONDS.sleep(1);
-            } catch (InterruptedException e) { e.printStackTrace(); }
-            System.out.println(threadName + ": after sleep");
+            System.out.println("    #"+Thread.currentThread().getName() + " finished task_" + taskCount++);
         };
         for(int i = 0; i < 5; i++){ threadPool.execute(task); }
         try{ TimeUnit.SECONDS.sleep(7);
