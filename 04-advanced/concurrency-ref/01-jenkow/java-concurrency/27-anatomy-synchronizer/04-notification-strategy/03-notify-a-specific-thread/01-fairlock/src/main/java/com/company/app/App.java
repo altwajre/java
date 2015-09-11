e1f8b12b5aa2e351b2d623f0fa349c0af8ec968e
William Han <whan@godaddy.com>
2015-09-11 00:56:53 -0700

    rename

--- 04-advanced/concurrency-ref/01-jenkow/java-concurrency/27-anatomy-synchronizer/04-notification-strategy/03-notify-a-specific-thread/01-fairlock/src/main/java/com/company/app/App.java
+++ 04-advanced/concurrency-ref/01-jenkow/java-concurrency/27-anatomy-synchronizer/04-notification-strategy/03-notify-a-specific-thread/01-fairlock/src/main/java/com/company/app/App.java
@@ -13,130 +13,81 @@ notified in a specific order, be it the order they called the synchronizer in, o
 To achieve this each waiting thread must call wait() on its own object. When the notifying thread wants to notify
 a specific waiting thread it will call notify() on the object this speicifc thread has called wait() on.
 
-queue:          thread1,       thread2,          thread3
-queue lockObj:  lockObj1(out), lockObj2,         lockObj3
-lockingThread:                          thread1(unlock),  thread2(unlock),  thread3
-
 output:
-    Thread_1; ENTER lock; lockObj.id=1
-    Thread_2; ENTER lock; lockObj.id=3
-    Thread_3; ENTER lock; lockObj.id=2
-#    Thread_2; ENTER while; isLocked=false; (queue[0] != queueObject)=true; queue.get(0).id=1; queueObject.id=3
-#    Thread_1; ENTER while; isLocked=false; (queue[0] != queueObject)=false; queue.get(0).id=1; queueObject.id=1
-  Thread_2; enter wait; lockObj.id=3
-#    Thread_3; ENTER while; isLocked=false; (queue[0] != queueObject)=true; queue.get(0).id=1; queueObject.id=2
-#    Thread_1 remove queueObject; queue.get(0).id=1; queueObject.id=1
-  Thread_3; enter wait; lockObj.id=2
-    Thread_1; unlock; lockObj.id=3
-  Thread_1; exit notify; lockObj.id=3
-Thread_1 finished; count=1
-  Thread_2; EXIT wait; lockObj.id=3
-#    Thread_2; ENTER while; isLocked=false; (queue[0] != queueObject)=false; queue.get(0).id=3; queueObject.id=3
-#    Thread_2 remove queueObject; queue.get(0).id=3; queueObject.id=3
-    Thread_2; unlock; lockObj.id=2
-  Thread_2; exit notify; lockObj.id=2
-Thread_2 finished; count=2
-  Thread_3; EXIT wait; lockObj.id=2
-#    Thread_3; ENTER while; isLocked=false; (queue[0] != queueObject)=false; queue.get(0).id=2; queueObject.id=2
-#    Thread_3 remove queueObject; queue.get(0).id=2; queueObject.id=2
-Thread_3 finished; count=3
+3
 
  */
-public class App {
-    static class Counter {
+public class App 
+{
+    static class Counter{
         public int count = 0;
         FairLock lock = new FairLock();
-
-        public void increment() {
+        public void increment(){
             lock.lock();
             try {
-                Thread.sleep(200);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            count++;  // critical section
+                Thread.sleep(100);
+            } catch (InterruptedException e) {  }
+            count++;
             lock.unlock();
-            System.out.println(Thread.currentThread().getName() + " finished; count=" + count);
         }
     }
-
-    static class QueueObject {
-        private int id = -1;
-        private boolean isNotified = false;
-
-        public QueueObject(int id) {
-            this.id = id;
-        }
-
-        public synchronized void doWait() {
-//            System.out.println("  "+Thread.currentThread().getName() + "; entered doWait; lockObj.id="+id);
-            while (!isNotified) {
-                try {
-                    System.out.println("  " + Thread.currentThread().getName() + "; enter wait; lockObj.id=" + id);
-                    this.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("  " + Thread.currentThread().getName() + "; EXIT wait; lockObj.id=" + id);
-            }
-            this.isNotified = false;
-//            System.out.println("  "+Thread.currentThread().getName() + "; exit doWait; lockObj.id="+id);
-        }
-
-        public synchronized void doNotify() {
-            this.isNotified = true;
-            this.notify();
-            System.out.println("  " + Thread.currentThread().getName() + "; exit notify; lockObj.id=" + id);
-        }
-    }
-
-    static int queueObjId = 1;
-
     static class FairLock {
-        private boolean isLocked = false;
-        private List<QueueObject> queue = new ArrayList<QueueObject>();
-
-        public void lock() {
-            QueueObject queueObject = new QueueObject(queueObjId++);
-            System.out.println("    " + Thread.currentThread().getName() + "; ENTER lock; lockObj.id=" + queueObject.id);
-            boolean isLockedForThisThread = true;
-            synchronized (this) {
-                queue.add(queueObject);
+        List<Object> lockObjs = new ArrayList<Object>();
+        boolean isLocked = false;
+        public void lock(){
+            Object lockObj = new Object();
+            synchronized (this){
+                lockObjs.add(lockObj);  // add threads lockObj to the queue
             }
-            while (isLockedForThisThread) {
-                System.out.println("#    " + Thread.currentThread().getName() + "; ENTER while;" + " isLocked=" + isLocked
-                        + "; (queue[0] != queueObject)=" + (queue.get(0) != queueObject) + "; queue.get(0).id=" + queue.get(0).id + "; queueObject.id="+ queueObject.id);
-                synchronized (this) {
-                    // 0 || 0 = 0; 0 || 1 = 1; 1 || 0 = 1; 1 || 1 = 1
-                    isLockedForThisThread = isLocked || queue.get(0) != queueObject; // non first objects in the queue need to wait
-                    if (!isLockedForThisThread) {
+            boolean mustWait = true;
+            while (mustWait){
+                synchronized (this){
+                    mustWait = isLocked || lockObjs.get(0) != lockObj;  // let go of the first thread
+                    if(!mustWait){
+                        if(lockObjs.size() > 0){
+                            lockObjs.remove(lockObj);
+                        }
                         isLocked = true;
-                        System.out.println("#    " + Thread.currentThread().getName() + " remove queueObject" + "; queue.get(0).id=" + queue.get(0).id + "; queueObject.id=" + queueObject.id);
-                        queue.remove(queueObject);
                         return;
                     }
                 }
-                queueObject.doWait();
+                synchronized (lockObj){
+                    if(mustWait){
+                        try {
+                            lockObj.wait();  // other threads need to wait
+                        } catch (InterruptedException e) {  }
+                    }
+                }
             }
         }
-
-        public synchronized void unlock() {
-            isLocked = false;
-            if (queue.size() > 0) {
-                System.out.println("    " + Thread.currentThread().getName() + "; unlock; lockObj.id=" + queue.get(0).id);
-                queue.get(0).doNotify();
+        public void unlock(){
+            if(lockObjs.size() > 0){
+                Object lockObj = lockObjs.get(0);
+                synchronized (lockObj){
+                    isLocked = false;
+                    lockObj.notify();
+                }
             }
         }
     }
-
-    public static void main(String[] args) {
-        Counter counter = new Counter();
-        for (int i = 1; i <= 3; i++) {
-            new Thread("Thread_" + i) {
-                public void run() {
+    public static void main( String[] args )
+    {
+        final Counter counter = new Counter();
+        int threadCount = 3;
+        for(int i = 1; i <= threadCount; i++){
+            new Thread("Thread_A_" + i){
+                public void run(){
                     counter.increment();
                 }
             }.start();
         }
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(counter.count);
     }
 }
--- 04-advanced/concurrency/04-lock/03-fairlock/01-simple/src/main/java/com/company/app/App.java
+++ 04-advanced/concurrency/04-lock/03-fairlock/01-simple/src/main/java/com/company/app/App.java
@@ -10,31 +10,6 @@ For how to implement the FairLock, see Slipped Conditions realistic example at l
 http://tutorials.jenkov.com/java-concurrency/slipped-conditions.html
 
 output:
-Thread_A_1 added signals: signal.id=1
-Thread_A_1 isLocked=false
-Thread_A_1 (signals.get(0) != signal) = false; signal.id=1
-Thread_A_1 mustWait=false
-Thread_A_1 removed signals: signal.id=1
-Thread_A_3 added signals: signal.id=2
-Thread_A_3 isLocked=true
-Thread_A_3 (signals.get(0) != signal) = false; signal.id=2
-Thread_A_3 mustWait=true
- Thread_A_3 signal.id=2 wait()
-Thread_A_2 added signals: signal.id=3
-Thread_A_2 isLocked=true
-Thread_A_2 (signals.get(0) != signal) = true; signal.id=3
-Thread_A_2 mustWait=true
- Thread_A_2 signal.id=3 wait()
-  Thread_A_1 signal.id=2 notify()
-Thread_A_3 isLocked=false
-Thread_A_3 (signals.get(0) != signal) = false; signal.id=2
-Thread_A_3 mustWait=false
-Thread_A_3 removed signals: signal.id=2
-  Thread_A_3 signal.id=3 notify()
-Thread_A_2 isLocked=false
-Thread_A_2 (signals.get(0) != signal) = false; signal.id=3
-Thread_A_2 mustWait=false
-Thread_A_2 removed signals: signal.id=3
 3
 
  */
@@ -42,7 +17,7 @@ public class App
 {
     static class Counter{
         public int count = 0;
-        Lock lock = new Lock();
+        FairLock lock = new FairLock();
         public void increment(){
             lock.lock();
             try {
@@ -52,57 +27,41 @@ public class App
             lock.unlock();
         }
     }
-    static int idCount = 1;
-    static class Signal {
-        public Signal(int id){
-            this.id = id;
-        }
-        public int id;
-    }
-    static class Lock{
-        List<Signal> signals = new ArrayList<Signal>();
+    static class FairLock {
+        List<Object> lockObjs = new ArrayList<Object>();
         boolean isLocked = false;
         public void lock(){
-            Signal signal;
+            Object lockObj = new Object();
             synchronized (this){
-                signal = new Signal(idCount++);
-                signals.add(signal);
-                System.out.println(Thread.currentThread().getName() + " added signals: signal.id=" + signal.id);
+                lockObjs.add(lockObj);  // add threads lockObj to the queue
             }
             boolean mustWait = true;
             while (mustWait){
                 synchronized (this){
-                    mustWait = isLocked || signals.get(0) != signal;
-                    System.out.println(Thread.currentThread().getName() + " isLocked=" + isLocked);
-                    System.out.println(Thread.currentThread().getName() + " (signals.get(0) != signal) = "
-                            + (signals.get(0) != signal) + "; signal.id="+ signal.id);
-                    System.out.println(Thread.currentThread().getName() + " mustWait=" + mustWait);
+                    mustWait = isLocked || lockObjs.get(0) != lockObj;  // let go of the first thread
                     if(!mustWait){
-                        if(signals.size() > 0){
-                            signals.remove(signal);
-                            System.out.println(Thread.currentThread().getName() + " removed signals: signal.id=" + signal.id);
+                        if(lockObjs.size() > 0){
+                            lockObjs.remove(lockObj);
                         }
                         isLocked = true;
                         return;
                     }
                 }
-                synchronized (signal){
+                synchronized (lockObj){
                     if(mustWait){
                         try {
-                            System.out.println(" "+Thread.currentThread().getName() + " signal.id=" + signal.id + " wait()");
-                            signal.wait();
+                            lockObj.wait();  // other threads need to wait
                         } catch (InterruptedException e) {  }
                     }
                 }
             }
         }
         public void unlock(){
-            if(signals.size() > 0){
-                Signal signal = signals.get(0);
-                synchronized (signal){
-                    System.out.println("  "+Thread.currentThread().getName() + " signal.id=" + signal.id + " notify()");
-                    signal.notify();
+            if(lockObjs.size() > 0){
+                Object lockObj = lockObjs.get(0);
+                synchronized (lockObj){
                     isLocked = false;
+                    lockObj.notify();
                 }
             }
         }
@@ -118,17 +77,11 @@ public class App
                 }
             }.start();
         }
-//        for(int i = 1; i <= threadCount; i++){
-//            new Thread("Thread_B_" + i){
-//                public void run(){
-//                    counter.increment();
-//                }
-//            }.start();
-//        }
 
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
         System.out.println(counter.count);
