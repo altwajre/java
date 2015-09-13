4b83086f6136e6abbc2e2d742fb3e3c32e8a7991
William Han <whan@godaddy.com>
2015-09-12 22:20:12 -0700

    add lock an object example

--- 04-advanced/concurrency-ref/java-util-concurrent/reentrantlock/02-lock-an-object/src/main/java/com/company/app/App.java
+++ 04-advanced/concurrency-ref/java-util-concurrent/reentrantlock/02-lock-an-object/src/main/java/com/company/app/App.java
@@ -3,45 +3,68 @@ package com.company.app;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.ReentrantLock;
 
 /*
 lock an object such as Chopstick
 
 output:
-chopstick.pickUp()=true
-chopstick.pickUp()=false
-chopstick.pickUp()=false
+pool-1-thread-1: Chopstick.pickUp()
+pool-1-thread-2: Lock.lock() wait()
 attempt to shutdown executor
-chopstick.pickUp()=true
+pool-1-thread-3: Lock.lock() wait()
 shutdown finished
+cancel non-finished tasks
+pool-1-thread-3: Lock.lock() wait()
+pool-1-thread-2: Lock.lock() wait()
 
  */
 public class App 
 {
     static class Chopstick{
-        public ReentrantLock lock = new ReentrantLock();
-        public boolean pickUp(){
-            return lock.tryLock();
+        public Lock lock = new Lock();
+        public void pickUp(){
+            lock.lock();
+            System.out.println(Thread.currentThread().getName() + ": Chopstick.pickUp()");
         }
         public void putDown(){
             lock.unlock();
+            System.out.println(Thread.currentThread().getName() + ": Chopstick.putDown()");
+        }
+    }
+    static class Lock{
+        boolean isLocked = false;
+        public void lock(){
+            synchronized (this){
+                while(isLocked){
+                    try {
+                        System.out.println(Thread.currentThread().getName() + ": Lock.lock() wait()");
+                        wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                isLocked = true;
+            }
+        }
+        public void unlock(){
+            synchronized (this){
+                notify();
+                isLocked = false;
+            }
         }
     }
     public static void main( String[] args )
     {
         Chopstick chopstick = new Chopstick();
         ExecutorService executor = Executors.newFixedThreadPool(3);
-        for(int i = 0; i < 3; i++){
-            executor.submit(() -> {
-                System.out.println("chopstick.pickUp()="+chopstick.pickUp());
-            });
-        }
         executor.submit(()->{
-            chopstick.putDown();
+            chopstick.pickUp();
         });
         executor.submit(()->{
-            System.out.println("chopstick.pickUp()=" + chopstick.pickUp());
+            chopstick.pickUp();
+        });
+        executor.submit(()->{
+            chopstick.pickUp();
         });
 
         stop(executor);
@@ -50,7 +73,7 @@ public class App
         try{
             System.out.println("attempt to shutdown executor");
             executor.shutdown();
-            executor.awaitTermination(5, TimeUnit.SECONDS);
+            executor.awaitTermination(1, TimeUnit.SECONDS);
         }
         catch (InterruptedException e){
             System.err.println("tasks interrupted");
---
+++ 04-advanced/concurrency-ref/java-util-concurrent/reentrantlock/03-trylock-an-object/src/main/java/com/company/app/App.java
@@ -0,0 +1,66 @@
+package com.company.app;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+/*
+trylock an object such as Chopstick
+
+output:
+chopstick.pickUp()=true
+chopstick.pickUp()=false
+chopstick.pickUp()=false
+attempt to shutdown executor
+chopstick.pickUp()=true
+shutdown finished
+
+ */
+public class App 
+{
+    static class Chopstick{
+        public ReentrantLock lock = new ReentrantLock();
+        public boolean pickUp(){
+            return lock.tryLock();
+        }
+        public void putDown(){
+            lock.unlock();
+        }
+    }
+    public static void main( String[] args )
+    {
+        Chopstick chopstick = new Chopstick();
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        for(int i = 0; i < 3; i++){
+            executor.submit(() -> {
+                System.out.println("chopstick.pickUp()="+chopstick.pickUp());
+            });
+        }
+        executor.submit(()->{
+            chopstick.putDown();
+        });
+        executor.submit(()->{
+            System.out.println("chopstick.pickUp()=" + chopstick.pickUp());
+        });
+
+        stop(executor);
+    }
+    static void stop(ExecutorService executor){
+        try{
+            System.out.println("attempt to shutdown executor");
+            executor.shutdown();
+            executor.awaitTermination(1, TimeUnit.SECONDS);
+        }
+        catch (InterruptedException e){
+            System.err.println("tasks interrupted");
+        }
+        finally {
+            if(!executor.isTerminated()){
+                System.err.println("cancel non-finished tasks");
+            }
+            executor.shutdownNow();
+            System.out.println("shutdown finished");
+        }
+    }
+}
