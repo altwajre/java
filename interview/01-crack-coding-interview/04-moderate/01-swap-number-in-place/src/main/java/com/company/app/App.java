b8336df0025ffc08089ec170ffab7698c2e4a4b9
William Han <whan@godaddy.com>
2015-09-19 09:07:46 -0700

    swap number in place example

---
+++ interview/01-crack-coding-interview/04-moderate/01-swap-number-in-place/src/main/java/com/company/app/App.java
@@ -0,0 +1,21 @@
+package com.company.app;
+
+/*
+ */
+public class App 
+{
+    static void swap(int a, int b){
+        // Example for a = 9, b = 4
+        a = a - b; // a = 9 - 4 = 5
+        b = a + b; // b = 5 + 4 = 9
+        a = b - a; // a = 9 - 5 = 4
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+    }
+    public static void main( String[] args )
+    {
+        int a = 1672;
+        int b = 9332;
+        swap(a, b);
+    }
+}
