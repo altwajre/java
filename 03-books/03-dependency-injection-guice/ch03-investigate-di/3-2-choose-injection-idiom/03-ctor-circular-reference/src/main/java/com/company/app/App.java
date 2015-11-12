737d067c94c11887f92b66ae38b44b81f7ed7e00
William Han <whan@godaddy.com>
2015-11-12 14:58:45 -0800

    add circular reference example

---
+++ 03-books/03-dependency-injection-guice/ch03-investigate-di/3-2-choose-injection-idiom/03-ctor-circular-reference/src/main/java/com/company/app/App.java
@@ -0,0 +1,41 @@
+package com.company.app;
+
+import com.google.inject.Guice;
+import com.google.inject.Inject;
+
+// 3.2.3. The circular reference problem
+public class App
+{
+    static class Host{
+        private final Symbiote symbiote;
+        @Inject
+        public Host(Symbiote symbiote){
+            this.symbiote = symbiote;
+        }
+        public void log(){
+            System.out.println("Host.log()");
+        }
+    }
+    static class Symbiote{
+        private final Host host;
+        @Inject
+        public Symbiote(Host host){
+            this.host = host;
+        }
+    }
+    /*
+    output:
+    Exception in thread "main" com.google.inject.ProvisionException: Unable to provision, see the following errors:
+
+    1) Tried proxying com.company.app.App$Host to support a circular dependency, but it is not an interface.
+      while locating com.company.app.App$Host
+        for parameter 0 at com.company.app.App$Symbiote.<init>(App.java:21)
+      while locating com.company.app.App$Symbiote
+        for parameter 0 at com.company.app.App$Host.<init>(App.java:11)
+      while locating com.company.app.App$Host
+     */
+    public static void main( String[] args )
+    {
+        Guice.createInjector().getInstance(Host.class).log();
+    }
+}
