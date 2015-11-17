05ada807985b4caad8f83505a538bee1e62f550b
William Han <whan@godaddy.com>
2015-11-16 23:36:31 -0800

    add factory structural example

---
+++ 02-design-patterns/01-creational-patterns/03-factory-method/01-structural/src/main/java/com/company/app/App.java
@@ -0,0 +1,38 @@
+package com.company.app;
+
+abstract class Product{}
+class ConcreteProductA extends Product{}
+class ConcreteProductB extends Product{}
+abstract class Creator{
+    public abstract Product factoryMethod();
+}
+class ConcreteCreatorA extends Creator{
+    @Override
+    public Product factoryMethod() {
+        return new ConcreteProductA();
+    }
+}
+class ConcreteCreatorB extends Creator{
+    @Override
+    public Product factoryMethod() {
+        return new ConcreteProductB();
+    }
+}
+public class App
+{
+    public static void main( String[] args )
+    {
+        Creator[] creators = new Creator[2];
+        creators[0] = new ConcreteCreatorA();
+        creators[1] = new ConcreteCreatorB();
+        for(Creator creator : creators){
+            Product product = creator.factoryMethod();
+            System.out.printf("Create %s\n", product.getClass().getSimpleName());
+        }
+    }
+}
+/*
+output:
+Create ConcreteProductA
+Create ConcreteProductB
+ */
---
+++ 02-design-patterns/01-creational-patterns/03-factory-method/02-real-world/src/main/java/com/company/app/App.java
@@ -0,0 +1,75 @@
+package com.company.app;
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class Page{  // abstract Product
+    @Override
+    public String toString(){ return this.getClass().getSimpleName(); }
+}
+class SkillsPage extends Page{}  // ConcreteProduct
+class EducationPage extends Page{}  // ConcreteProduct
+class ExperiencePage extends Page{}  // ConcreteProduct
+class IntroductionPage extends Page{}  // ConcreteProduct
+class ResultsPage extends Page{}  // ConcreteProduct
+class ConclusionPage extends Page{}  // ConcreteProduct
+class SummaryPage extends Page{}  // ConcreteProduct
+class BibliographyPage extends Page{}  // ConcreteProduct
+abstract class Document  // abstract Creator
+{
+    public Document(){ this.createPages(); }
+    protected List<Page> pages;
+    public abstract void createPages();
+    @Override
+    public String toString(){ return this.getClass().getSimpleName();}
+}
+class Resume extends Document{  // Concrete Creator
+    @Override
+    public void createPages() {
+        this.pages = new ArrayList<Page>(){{
+            add(new SkillsPage());
+            add(new EducationPage());
+            add(new ExperiencePage());
+        }};
+    }
+}
+class Report extends Document{  // Concrete Creator
+    @Override
+    public void createPages() {
+        this.pages = new ArrayList<Page>();
+        this.pages.add(new IntroductionPage());
+        this.pages.add(new ResultsPage());
+        this.pages.add(new ConclusionPage());
+        this.pages.add(new SummaryPage());
+        this.pages.add(new BibliographyPage());
+    }
+}
+public class App
+{
+    public static void main( String[] args )
+    {
+        List<Document> documents = new ArrayList<Document>();
+        documents.add(new Resume());
+        documents.add(new Report());
+        for(Document document : documents){
+            System.out.println(document + "--");
+            for(Page page : document.pages){
+                System.out.println(" " + page);
+            }
+            System.out.println("");
+        }
+    }
+}
+/*
+output:
+Resume--
+ SkillsPage
+ EducationPage
+ ExperiencePage
+
+Report--
+ IntroductionPage
+ ResultsPage
+ ConclusionPage
+ SummaryPage
+ BibliographyPage
+ */
