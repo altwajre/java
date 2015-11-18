78af9a3047de16e955aff4c09b24bbdd70a27f94
William Han <whan@godaddy.com>
2015-11-17 18:31:23 -0800

    add flyweight structural example

---
+++ 02-design-patterns/02-structural-patterns/06-flyweight/02-real-world/src/main/java/com/company/app/App.java
@@ -0,0 +1,90 @@
+package com.company.app;
+import java.util.HashMap;
+import java.util.Map;
+
+class Char {  // Flyweight
+    protected char symbol;
+    protected Integer width;
+    protected Integer height;
+    protected Integer ascent;
+    protected Integer descent;
+    public void display(Integer pointSize){
+        System.out.format("%s (pointsize %s)\n", this.symbol, pointSize);
+    }
+}
+class CharA extends Char {  // Concrete Flyweight
+    public CharA(){
+        this.symbol = 'A';
+        this.height = 100;
+        this.width = 120;
+        this.ascent = 70;
+        this.descent = 0;
+    }
+}
+class CharB extends Char {  // Concrete Flyweight
+    public CharB(){
+        this.symbol = 'B';
+        this.height = 100;
+        this.width = 140;
+        this.ascent = 72;
+        this.descent = 0;
+    }
+}
+class CharZ extends Char {  // Concrete Flyweight
+    public CharZ(){
+        this.symbol = 'Z';
+        this.height = 100;
+        this.width = 100;
+        this.ascent = 68;
+        this.descent = 0;
+    }
+}
+class CharFactory{  // FlyweightFactory
+    private Map<Character, Char> characters = new HashMap<Character, Char>();
+    public Char getChar(Character key){
+        // Uses "lazy initialization"
+        Char character = null;
+        if(characters.containsKey(key)){
+            character = characters.get(key);
+        }
+        else{
+            switch (key){
+                case 'A': character = new CharA(); break;
+                case 'B': character = new CharB(); break;
+                //...
+                case 'Z': character = new CharZ(); break;
+            }
+            characters.put(key, character);
+        }
+        return character;
+    }
+}
+public class App
+{
+    public static void main( String[] args )
+    {
+        String document = "AAZZBBZB";
+        char[] chars = document.toCharArray();
+        CharFactory factory = new CharFactory();
+        int pointSize = 10;
+        for(char c : chars){
+            pointSize++;
+            Char character = factory.getChar(c);
+            character.display(pointSize);
+        }
+    }
+}
+/*
+Definition
+Use sharing to support large numbers of fine-grained objects efficiently.
+
+output:
+A (pointsize 11)
+A (pointsize 12)
+Z (pointsize 13)
+Z (pointsize 14)
+B (pointsize 15)
+B (pointsize 16)
+Z (pointsize 17)
+B (pointsize 18)
+ */
