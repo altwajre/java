73bef73bbcaa46be745fef7695168dad44c366c9
William Han <whan@godaddy.com>
2016-01-08 09:52:15 -0800

    add search replace regex examples

---
+++ essential/06-regex/01-search-replace/src/main/java/com/company/app/App.java
@@ -0,0 +1,15 @@
+package com.company.app;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        String env = "test";
+        String queryString = "/?ci=13931&iphoneview=1";
+        String url = "https://www.godaddy.com";
+        url = url.replaceFirst("www.", "www." + env + "-");
+        url = url.replaceFirst(".com", ".com" + queryString);
+        System.out.println(url);
+
+    }
+}
