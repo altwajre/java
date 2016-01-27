ffc7ff4f1e0afd57c879f88f12150c0b12b4f3fe
William Han <whan@godaddy.com>
2016-01-26 18:03:53 -0800

    update memory model example

--- 04-advanced/concurrency-ref/01-jenkow/java-concurrency/10-memory-model/src/main/java/com/company/app/App.java
+++ 04-advanced/concurrency-ref/01-jenkow/java-concurrency/10-memory-model/src/main/java/com/company/app/App.java
@@ -1,52 +1,39 @@
 package com.company.app;
 
 /*
- http://tutorials.jenkov.com/java-concurrency/java-memory-model.html
- http://tutorials.jenkov.com/images/java-concurrency/java-memory-model-3.png
-
-    output:
-    thread_1 - SharedObject.object2 before add: 22
-    thread_2 - SharedObject.object2 before add: 22
-    thread_2 - SharedObject.object2 after add: 26
-    SharedObject.sharedInstance: 22
-    thread_1 - SharedObject.object2 after add: 24
-
-    SharedObject.sharedInstance: 22
-    thread_2 - SharedObject.object2 before add: 22
-    thread_1 - SharedObject.object2 before add: 22
-    thread_2 - SharedObject.object2 after add: 24
-    thread_1 - SharedObject.object2 after add: 26
+http://tutorials.jenkov.com/java-concurrency/java-memory-model.html
+http://tutorials.jenkov.com/images/java-concurrency/java-memory-model-3.png
  */
+class SharedObject{
+    // static variable pointing to instance of SharedObject
+    public static final SharedObject sharedInstance = new SharedObject();  // object3
+    // member variable pointing to two objects on the heap
+    public Integer object2 = new Integer(22);
+    public Integer object4 = new Integer(44);
+    public long member1 = 12345;
+    public long member2 = 67890;
+}
+class MyRunnable implements Runnable{
+    public void run() {
+        methodOne();
+    }
+    public void methodOne(){
+        String threadName = Thread.currentThread().getName();
+        int localVariable1 = 45;
+        SharedObject localVariable2 = SharedObject.sharedInstance;
+        System.out.println(threadName + " - SharedObject.object2 before add: " + localVariable2.object2);
+        localVariable2.object2 += 2; // add 2 on each thread
+        System.out.println(threadName + " - SharedObject.object2 after add: " + localVariable2.object2);
+        // do more with local variables
+        methodTwo();
+    }
+    public void methodTwo(){
+        Integer localVariable1 = new Integer(99); // object1 and object5
+        // do more with local variable
+    }
+}
 public class App
 {
-    static class SharedObject{
-        // static variable pointing to instance of SharedObject
-        public static final SharedObject sharedInstance = new SharedObject();  // object3
-        // member variable pointing to two objects on the heap
-        public Integer object2 = new Integer(22);
-        public Integer object4 = new Integer(44);
-        public long member1 = 12345;
-        public long member2 = 67890;
-    }
-    static class MyRunnable implements Runnable{
-        public void run() {
-            methodOne();
-        }
-        public void methodOne(){
-            String threadName = Thread.currentThread().getName();
-            int localVariable1 = 45;
-            SharedObject localVariable2 = SharedObject.sharedInstance;
-            System.out.println(threadName + " - SharedObject.object2 before add: " + localVariable2.object2);
-            localVariable2.object2 += 2; // add 2 on each thread
-            System.out.println(threadName + " - SharedObject.object2 after add: " + localVariable2.object2);
-            // do more with local variables
-            methodTwo();
-        }
-        public void methodTwo(){
-            Integer localVariable1 = new Integer(99); // object1 and object5
-            // do more with local variable
-        }
-    }
     public static void main( String[] args )
     {
         SharedObject so1 = SharedObject.sharedInstance;
@@ -59,3 +46,17 @@ public class App
         System.out.println("SharedObject.sharedInstance: " + SharedObject.sharedInstance.object2);
     }
 }
+/*
+output:
+thread_1 - SharedObject.object2 before add: 22
+thread_2 - SharedObject.object2 before add: 22
+thread_2 - SharedObject.object2 after add: 26
+SharedObject.sharedInstance: 22
+thread_1 - SharedObject.object2 after add: 24
+
+SharedObject.sharedInstance: 22
+thread_2 - SharedObject.object2 before add: 22
+thread_1 - SharedObject.object2 before add: 22
+thread_2 - SharedObject.object2 after add: 24
+thread_1 - SharedObject.object2 after add: 26
+ */
