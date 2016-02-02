b109ec3cd7023b35506a5d391bff1cb23980d1ea
William Han <whan@godaddy.com>
2016-02-02 10:39:11 -0800

    add atomicinteger

---
+++ 04-advanced/concurrency/02-reference/01-jenkow/java-concurrency-util/23-atomicinteger/src/main/java/com/company/app/App.java
@@ -0,0 +1,68 @@
+package com.company.app;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/*
+http://tutorials.jenkov.com/java-util-concurrent/atomicinteger.html
+
+The AtomicInteger class provides you with a int variable which can be read and written atomically, and which also
+contains advanced atomic operations like compareAndSet().
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        testGetValue();
+        testSetValue();
+        testGetAndSet();
+        testCompareAndSet();
+    }
+
+    private static void testCompareAndSet() {
+        System.out.println("#testCompareAndSet");
+        AtomicInteger atomicInteger = new AtomicInteger(123);
+        int expectedValue = 123;
+        int newValue = 789;
+        boolean wasNewValueSet = atomicInteger.compareAndSet(expectedValue, newValue);
+        System.out.println("wasNewValueSet="+wasNewValueSet);
+        int value = atomicInteger.get();
+        System.out.println("value="+value);
+    }
+
+    private static void testGetAndSet() {
+        System.out.println("#testGetAndSet");
+        AtomicInteger atomicInteger = new AtomicInteger(123);
+        int oldValue = atomicInteger.getAndSet(789);
+        System.out.println("oldValue="+oldValue);
+        int newValue = atomicInteger.get();
+        System.out.println("newValue="+newValue);
+    }
+
+    private static void testSetValue() {
+        System.out.println("#testSetValue");
+        AtomicInteger atomicInteger = new AtomicInteger(123);
+        atomicInteger.set(789);
+        int value = atomicInteger.get();
+        System.out.println("value="+value);
+    }
+
+    private static void testGetValue() {
+        System.out.println("#testGetValue");
+        AtomicInteger atomicInteger = new AtomicInteger(123);
+        int value = atomicInteger.get();
+        System.out.println("value="+value);
+    }
+}
+/*
+output:
+#testGetValue
+value=123
+#testSetValue
+value=789
+#testGetAndSet
+oldValue=123
+newValue=789
+#testCompareAndSet
+wasNewValueSet=true
+value=789
+ */
