2785a5c1e1a96ee52b52e99b597a363cc464a887
William Han <whan@godaddy.com>
2016-04-22 16:30:39 -0700

    add hibernate basics

---
+++ 04-data/01-hibernate/02-videos/01-hibernate-and-java-persistence-api/03-hibernate-basics/persistence-methods/README.md
@@ -0,0 +1,12 @@
+# hibernate basics - persistence methods
+
+## Run the App
+
+com.company.app.data.App.main()
+
+## MySQL
+
+```
+-- use ifinances;
+select * from FINANCES_USER;
+```
---
+++ 04-data/01-hibernate/02-videos/01-hibernate-and-java-persistence-api/03-hibernate-basics/persistence-methods/src/main/java/com/company/app/data/HibernateUtil.java
@@ -0,0 +1,20 @@
+package com.company.app.data;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+    private static SessionFactory buildSessionFactory(){
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        return configuration
+                .buildSessionFactory(new StandardServiceRegistryBuilder()
+                .applySettings(configuration.getProperties())
+                .build());
+    }
+    public static SessionFactory getSessionFactory(){
+        return sessionFactory;
+    }
+}
