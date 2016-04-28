ca671185441ddefbe9ab6b47262b25cee76b8b18
William Han <whan@godaddy.com>
2016-04-28 15:17:59 -0700

    add lombok readme

--- maven-repository/01-lombok/04-@getter-and-@setter/src/main/java/com/company/app/App.java
+++ maven-repository/01-lombok/04-@getter-and-@setter/src/main/java/com/company/app/App.java
@@ -1,24 +1,15 @@
 package com.company.app;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.Setter;
-
-class GetterSetterExample{
-    @Getter
-    @Setter
-    private int age = 10;
-
-    @Setter(AccessLevel.PROTECTED)
-    private String name;
-
-    @Override public String toString(){
-        return String.format("%s (age: %d)", name, age);
-    }
-}
 public class App {
     public static void main(String[] args){
         GetterSetterExample getterSetterExample = new GetterSetterExample();
+
+        getterSetterExample.setAge(28);
+        System.out.println(getterSetterExample.getAge());
+
+        getterSetterExample.setName("Tom");
+
+        System.out.println(getterSetterExample);
     }
 }
 /*
---
+++ maven-repository/01-lombok/README.md
@@ -0,0 +1,9 @@
+# lombok
+
+https://projectlombok.org/
+
+## IntelliJ IDEA
+
+1, One time install the lombok plugin before using it
+
+2, Check `Enable annotation processing` checkbox in Settings->Compiler->Annotation Processors
