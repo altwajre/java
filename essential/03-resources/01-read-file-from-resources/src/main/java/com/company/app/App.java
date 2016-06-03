42eed4841aad9ca063e3c61fae8bef5b38b1070f
William Han <whan@godaddy.com>
2016-06-02 22:22:05 -0700

    add resources

---
+++ essential/03-resources/01-read-file-from-resources/src/main/java/com/company/app/App.java
@@ -0,0 +1,24 @@
+package com.company.app;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class App
+{
+    public static void main( String[] args ) throws IOException {
+        ClassLoader classLoader = App.class.getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("test.xml");
+        String result = IOUtils.toString(inputStream, "UTF-8");
+        System.out.println(result);
+    }
+}
+/*
+output:
+
+<Person>
+    <name>Tom</name>
+    <age>10</age>
+</Person>
+ */
--- test/22-testng/11-test-runner/02-set-xml-suites/src/main/java/com/company/app/App.java
+++ test/22-testng/11-test-runner/02-set-xml-suites/src/main/java/com/company/app/App.java
@@ -5,16 +5,20 @@ import org.testng.xml.Parser;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class App
 {
     public static void main( String[] args )
     {
         TestNG testNG = new TestNG();
-        String xml = "<suite name=\"test-all\"><test name=\"Regression\" group-by-instances=\"true\"><packages><package name=\"com.company.app.test\" /></packages></test></suite>";
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(xml.getBytes());
+
+//        String xml = "<suite name=\"test-all\"><test name=\"Regression\" group-by-instances=\"true\"><packages><package name=\"com.company.app.test\" /></packages></test></suite>";
+//        ByteArrayInputStream inputStream = new ByteArrayInputStream(xml.getBytes());
+
+        ClassLoader classLoader = App.class.getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("test-all.xml");
 
         try {
             testNG.setXmlSuites(new Parser(inputStream).parseToList());
@@ -29,3 +33,14 @@ public class App
         testNG.run();
     }
 }
+/*
+output:
+[TestNG] Running:
+  /Users/whan/Desktop/github/java-repo/7-2/java/test/22-testng/11-test-runner/02-set-xml-suites/testng.xml
+
+
+===============================================
+test-all
+Total tests run: 2, Failures: 1, Skips: 0
+===============================================
+ */
