5c0655921e0184b3482a6d2107da291ca0f2113a
William Han <whan@godaddy.com>
2016-11-06 22:15:57 -0800

    add cookbook.sql

--- 88-databases/01-sql/01-jdbc/02-books/02-mysql-cookbook/ch02-mysql-programs/07-identify-null-in-resultset/java/src/main/java/com/company/app/App.java
+++ 88-databases/01-sql/01-jdbc/02-books/02-mysql-cookbook/ch02-mysql-programs/07-identify-null-in-resultset/java/src/main/java/com/company/app/App.java
@@ -2,11 +2,7 @@ package com.company.app;
 
 import java.sql.*;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
--- 88-databases/01-sql/01-jdbc/02-books/02-mysql-cookbook/cookbook.sql
+++ 88-databases/01-sql/01-jdbc/02-books/02-mysql-cookbook/cookbook.sql
@@ -1,3 +1,11 @@
+DROP DATABASE cookbook;
+CREATE DATABASE cookbook;
+
+USE cookbook;
+
+CREATE USER 'cbuser'@'localhost' IDENTIFIED BY 'cbpass';
+GRANT ALL ON cookbook.* TO 'cbuser'@'localhost'; -- Grant cbuser to cookbook database
+SELECT user FROM mysql.user;  -- show users
 
 DROP TABLE IF EXISTS profile;
 
