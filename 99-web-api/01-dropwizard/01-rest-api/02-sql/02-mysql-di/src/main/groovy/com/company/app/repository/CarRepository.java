3d85ce06b813677f4322356216252395aa7fa377
William Han <whan@godaddy.com>
2016-11-18 08:58:15 -0800

    rename

--- 99-web-api/01-dropwizard/01-rest-api/02-sql/02-mysql-di/src/main/groovy/com/company/app/repository/CarRepository.java
+++ 99-web-api/01-dropwizard/01-rest-api/02-sql/02-mysql-di/src/main/groovy/com/company/app/repository/CarRepository.java
@@ -11,6 +11,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+// Repository for stored procedure data access
 public class CarRepository {
     DataSourceConfig config;
 
