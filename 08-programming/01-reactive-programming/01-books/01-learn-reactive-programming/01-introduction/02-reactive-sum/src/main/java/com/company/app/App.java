122e6f050a19b93a1dc1f03bcb3c78e767b3eab1
William Han <whan@godaddy.com>
2017-01-27 11:00:46 -0800

    update reactive sum

--- 08-programming/01-reactive-programming/01-books/01-learn-reactive-programming/01-introduction/02-reactive-sum/src/main/java/com/company/app/App.java
+++ 08-programming/01-reactive-programming/01-books/01-learn-reactive-programming/01-introduction/02-reactive-sum/src/main/java/com/company/app/App.java
@@ -10,7 +10,9 @@ import rx.observables.ConnectableObservable;
 import rx.schedulers.Schedulers;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.concurrent.CountDownLatch;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -20,21 +22,24 @@ public class App {
     public static final class ReactiveSum implements Observer<Double> {
 
         private CountDownLatch latch = new CountDownLatch(1);
+
         private double sum;
         private Subscription subscription = null;
 
         public ReactiveSum(Observable<Double> a, Observable<Double> b) {
             this.sum = 0;
+
+            subscribe(a, b);
         }
 
         private void subscribe(Observable<Double> a, Observable<Double> b) {
+            // combineLatest creates an Observable, sending notifications on changes of either of its sources.
+            // This notifications are formed using a Func2.
             this.subscription = Observable.combineLatest(a, b, new Func2<Double, Double, Double>() {
-                @Override
                 public Double call(Double a, Double b) {
                     return a + b;
                 }
             }).subscribeOn(Schedulers.io()).subscribe(this);
-
         }
 
         public void unsubscribe() {
@@ -42,20 +47,17 @@ public class App {
             this.latch.countDown();
         }
 
-        @Override
         public void onCompleted() {
-            System.out.println("Exiting last sum was: " + this.sum);
+            System.out.println("Exiting last sum was : " + this.sum);
             this.latch.countDown();
         }
 
-        @Override
         public void onError(Throwable e) {
             System.err.println("Got an error!");
             e.printStackTrace();
         }
 
-        @Override
-        public void onNext(Double aDouble) {
+        public void onNext(Double sum) {
             this.sum = sum;
             System.out.println("update : a + b = " + sum);
         }
@@ -65,7 +67,14 @@ public class App {
         }
     }
 
-    public static Observable<Double> varStream(final String varName, Observable<String> input) {
+    /*
+    The Observable returned by this method, only reacts to values in the form
+    <varName> = <value> or <varName> : <value>.
+    It emits the <value>.
+     */
+
+    public static Observable<Double> varStream(final String varName,
+                                               Observable<String> input) {
         final Pattern pattern = Pattern.compile("^\\s*" + varName
                 + "\\s*[:|=]\\s*(-?\\d+\\.?\\d*)$");
 
@@ -92,21 +101,59 @@ public class App {
         });
     }
 
-    public static ConnectableObservable<String> from(final InputStream stream){
-        return null;
+    public static ConnectableObservable<String> from(final InputStream stream) {
+        return from(new BufferedReader(new InputStreamReader(stream)));
     }
 
-    public static ConnectableObservable<String> from(final BufferedReader reader){
+    public static ConnectableObservable<String> from(final BufferedReader reader) {
         return Observable.create((Subscriber<? super String> subscriber) -> {
+            try {
+                String line;
 
+                if (subscriber.isUnsubscribed()) {
+                    return;
+                }
+
+                while (!subscriber.isUnsubscribed() && (line = reader.readLine()) != null) {
+                    if (line.equals("exit")) {
+                        break;
+                    }
+
+                    subscriber.onNext(line);
+                }
+            } catch (IOException e) {
+                subscriber.onError(e);
+            }
+
+            if (!subscriber.isUnsubscribed()) {
+                subscriber.onCompleted();
+            }
         }).publish();
     }
 
     public static void main(String... args) {
-        System.out.println("Reactive Sum. Type 'a: <number>' and 'b: <number>' to try ti");
 
-        ConnectableObservable<String> input = CreateObservable.from(System.in);
+        System.out.println("Reactive Sum. Type 'a: <number>' and 'b: <number>' to try it");
+
+        ConnectableObservable<String> input = from(System.in);
+
+        Observable<Double> a = varStream("a", input);
+        Observable<Double> b = varStream("b", input);
+
+        ReactiveSum sum = new ReactiveSum(a, b);
+
+        input.connect();
+
+        try {
+            sum.getLatch().await();
+        } catch (InterruptedException e) {}
     }
-
-
 }
+/*
+output:
+Reactive Sum. Type 'a: <number>' and 'b: <number>' to try it
+a: 2
+b: 3
+update : a + b = 5.0
+ */
+
