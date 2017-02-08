3dd9d63535e6d02ba9468b87220df35bde6a97d2
William Han <whan@godaddy.com>
2017-02-08 15:00:39 -0800

    update

--- 08-programming/03-dynamic-programming/01-fibonacci/src/main/java/com/company/app/App.java
+++ 08-programming/03-dynamic-programming/01-fibonacci/src/main/java/com/company/app/App.java
@@ -3,8 +3,7 @@ package com.company.app;
 import java.util.HashMap;
 import java.util.Map;
 
-public class App
-{
+public class App {
     /*
                                                        (9)r34
                                                 (8)r21  +  (7)r13
@@ -20,11 +19,11 @@ public class App
       fibonacci: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
       index:     0  1  2  3  4  5  6  7   8   9
      */
-    static int naiveRecursive(int n){
+    static int naiveRecursive(int n) {
         int f = 0;
         if (n <= 2) f = 1;
         else {
-            f = naiveRecursive(n -1) + naiveRecursive(n -2);
+            f = naiveRecursive(n - 1) + naiveRecursive(n - 2);
         }
         return f;
     }
@@ -34,13 +33,13 @@ public class App
       fibonacci: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
       index:     0  1  2  3  4  5  6  7   8   9
      */
-    static int iteration(int numberOfTerms){
+    static int iteration(int numberOfTerms) {
         int first = 0;
         int second = 1;
         int next = 0;
 //        System.out.print("fibonacci: " + first + ", " + second);
         int thirdIndex = 2;
-        for(int i = thirdIndex; i <= numberOfTerms; i++){
+        for (int i = thirdIndex; i <= numberOfTerms; i++) {
             next = first + second;
 //            System.out.print(", " + next);
             first = second;
@@ -50,35 +49,72 @@ public class App
     }
 
     // https://www.youtube.com/watch?v=OQ5jsbhAv_M&list=PLfMspJ0TLR5HRFu2kLh3U4mvStMO8QURm
-    static int memoizedDpRecursive(int n, Map<Integer, Integer> memo){
+    static int memoizedDpRecursive(int n, Map<Integer, Integer> memo) {
         int f = 0;
-        if(memo.containsKey(n)) return memo.get(n);
+        if (memo.containsKey(n)) return memo.get(n);
         if (n <= 2) f = 1;
         else {
-            f = memoizedDpRecursive(n - 1, memo) + memoizedDpRecursive(n -2, memo);
+            f = memoizedDpRecursive(n - 1, memo) + memoizedDpRecursive(n - 2, memo);
         }
         memo.put(n, f);
         return f;
     }
 
-    public static void main( String[] args )
-    {
-        System.out.println("Iteration");
-        System.out.println(iteration(9));
+    // iteration sum below is not dynamic programming. Maybe dynamic programming only apply to recursive
+    static int iterationSum(int n) {
+        int[] buffer = new int[n];
+        buffer[0] = 0;
+        buffer[1] = 1;
+        for (int i = 2; i < n; i++) {
+            buffer[i] = buffer[i - 1] + buffer[i - 2];
+        }
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += buffer[i];
+        }
+
+        return sum;
+    }
+
+    // iteration below is not dynamic programming. Maybe dynamic programming only apply to recursive
+    static int iteration2(int n) {
+        int[] buffer = new int[n];
+        buffer[0] = 0;
+        buffer[1] = 1;
+        for (int i = 2; i < n; i++) {
+            buffer[i] = buffer[i - 1] + buffer[i - 2];
+        }
+
+        return buffer[n - 1] + buffer[n - 2]; // add last two items
+    }
+
+    public static void main(String[] args) {
 
         System.out.println("NaiveRecursive");
         System.out.println(naiveRecursive(9));
 
+        System.out.println("Iteration");
+        System.out.println(iteration(9));
+        System.out.println("iteration2");
+        System.out.println(iteration2(9));
+        System.out.println("iterationSum");
+        System.out.println(iterationSum(9));
+
         System.out.println("MemoizedDpRecursive");
         System.out.println(memoizedDpRecursive(9, new HashMap<Integer, Integer>()));
     }
-    /*
-    output:
-    Iteration
-    34
-    NaiveRecursive
-    34
-    MemoizedDpRecursive
-    34
-     */
 }
+/*
+output:
+NaiveRecursive
+34
+Iteration
+34
+iteration2
+34
+iterationSum
+54
+MemoizedDpRecursive
+34
+ */
