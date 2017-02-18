f34ed3211c0f1537d0978b15da78bf6ef3d79d9b
William Han <whan@godaddy.com>
2017-02-17 22:32:21 -0800

    add string literals

---
+++ 06-regex/03-documentation/03-string-literals/src/main/java/com/company/app/App.java
@@ -0,0 +1,75 @@
+package com.company.app;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        test1();
+
+        test2();
+
+        test3();
+    }
+
+    private static void test3() {
+        System.out.println("#test3");
+        String regex = "cat."; // "." means "any character", metacharacters: <([{\^-=$!|]})?*+.>
+        String input = "cats";
+
+        testRegex(regex, input);
+    }
+/*
+output:
+#test3
+I found the text $cats$ starting at index 0 and ending at index 4.
+ */
+
+    private static void test2() {
+        System.out.println("#test2");
+        String regex = "foo";
+        String input = "foofoofoo";
+
+        testRegex(regex, input);
+    }
+/*
+output:
+#test2
+I found the text $foo$ starting at index 0 and ending at index 3.
+I found the text $foo$ starting at index 3 and ending at index 6.
+I found the text $foo$ starting at index 6 and ending at index 9.
+ */
+
+    private static void test1() {
+        System.out.println("#test1");
+        String regex = "foo";
+        String input = "foo";
+
+        testRegex(regex, input);
+    }
+/*
+output:
+#test1
+I found the text $foo$ starting at index 0 and ending at index 3.
+ */
+
+    private static void testRegex(String regex, String input) {
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher matcher = pattern.matcher(input);
+
+        boolean found = false;
+        while (matcher.find()) {
+            System.out.format("I found the text $%s$ starting at index %d and ending at index %d.%n",
+                    matcher.group(), matcher.start(), matcher.end());
+//            System.out.println(matcher.group(1));
+            found = true;
+        }
+
+        if (!found) {
+            System.out.format("No match found.%n");
+        }
+    }
+}
---
+++ 06-regex/11-find/src/main/java/com/company/app/App.java
@@ -0,0 +1,41 @@
+package com.company.app;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        String target = "order_id=";
+//        String target = "Shopper_ID=";
+
+        String regex = String.format("\\s+%s\"([^\"]+)", target);
+//        String regex = "\\s+order_id='([^']+)";
+        String input = "<html><body><title order_id=\"cool\"></body></html>";
+//        String input = "<html><body><return>&lt; Shopper_ID=\"1366898\"/&gt;</return></body></html>";
+//        String regex = "\\Gdog";
+//        String input = "dogdog";
+
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher matcher = pattern.matcher(input);
+
+        String targetValue = null;
+        boolean found = false;
+        while (matcher.find()) {
+            System.out.format("I found the text $%s$ starting at index %d and ending at index %d.%n",
+                    matcher.group(), matcher.start(), matcher.end());
+            targetValue = matcher.group(1);
+            found = true;
+        }
+
+        if (!found) {
+            System.out.format("No match found.%n");
+        }
+        else{
+            System.out.println("#order_id="+targetValue);
+        }
+
+    }
+}
