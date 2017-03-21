24e3b40e981cf0712d1111c2728eccccf75eb05c
William Han <whan@godaddy.com>
2017-03-20 23:09:26 -0700

    update load from resource

--- 77-data/01-xml/01-xom/01-load-xml-from-resources/src/main/java/com/company/app/App.java
+++ 77-data/01-xml/01-xom/01-load-xml-from-resources/src/main/java/com/company/app/App.java
@@ -3,6 +3,8 @@ package com.company.app;
 import nu.xom.Builder;
 import nu.xom.Document;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
 
 /*
@@ -11,21 +13,49 @@ load xml from resources
 public class App
 {
     public static void main( String[] args ) throws Exception {
-        ClassLoader classLoader = App.class.getClassLoader();
-        InputStream inputStream = classLoader.getResourceAsStream("test.xml");
+        classLoader_test();
+
+        inputStream_test();
+    }
+
+    private static void inputStream_test() throws Exception {
+        System.out.println("#inputStream_test");
+
+        InputStream inputStream = new FileInputStream(new File("src/main/resources/xml/test.xml"));
 
         Builder builder = new Builder();
         Document document = builder.build(inputStream);
 
         System.out.println(document.toXML());
     }
-}
 /*
 output:
-
+#inputStream_test
 <?xml version="1.0"?>
 <Person>
     <name>Tom</name>
     <age>10</age>
 </Person>
  */
+
+    private static void classLoader_test() throws Exception {
+        System.out.println("#classLoader_test");
+
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("xml/test.xml");
+
+        Builder builder = new Builder();
+        Document document = builder.build(inputStream);
+
+        System.out.println(document.toXML());
+    }
+/*
+output:
+#classLoader_test
+<?xml version="1.0"?>
+<Person>
+    <name>Tom</name>
+    <age>10</age>
+</Person>
+ */
+}
--- 77-data/02-json/01-load-json-from-resources/src/main/java/com/company/app/App.java
+++ 77-data/02-json/01-load-json-from-resources/src/main/java/com/company/app/App.java
@@ -2,6 +2,8 @@ package com.company.app;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
@@ -9,7 +11,36 @@ import java.util.Map;
 public class App
 {
     public static void main( String[] args ) throws IOException {
-        ClassLoader classLoader = App.class.getClassLoader();
+        classLoader_test();
+
+        inputStream_test();
+    }
+
+    private static void inputStream_test() throws IOException {
+        System.out.println("#inputStream_test");
+
+        InputStream inputStream = new FileInputStream(new File("src/main/resources/json/test.json"));
+        Map mapJson = new ObjectMapper().readValue(inputStream, Map.class);
+        System.out.println(mapJson);
+
+        Map classJson = (Map)mapJson.get("class");
+        System.out.println(classJson);
+
+        Map studentsJson = (Map)classJson.get("students");
+        System.out.println(studentsJson);
+    }
+/*
+output:
+#inputStream_test
+{class={name=Math, students={123=Tom, 234=Jen}}}
+{name=Math, students={123=Tom, 234=Jen}}
+{123=Tom, 234=Jen}
+ */
+
+    private static void classLoader_test() throws IOException {
+        System.out.println("#classLoader_test");
+
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         InputStream inputStream = classLoader.getResourceAsStream("json//test.json");
         Map mapJson = new ObjectMapper().readValue(inputStream, Map.class);
         System.out.println(mapJson);
@@ -20,23 +51,11 @@ public class App
         Map studentsJson = (Map)classJson.get("students");
         System.out.println(studentsJson);
     }
-}
 /*
-input:
-{
-  "class":
-  {
-    "name": "Math",
-    "students":
-    {
-      "123": "Tom",
-      "234": "Jen"
-    }
-  }
-}
-
 output:
+#classLoader_test
 {class={name=Math, students={123=Tom, 234=Jen}}}
 {name=Math, students={123=Tom, 234=Jen}}
 {123=Tom, 234=Jen}
  */
+}
