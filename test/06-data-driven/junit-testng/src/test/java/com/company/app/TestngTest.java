022ee3fa5452235ac046c0712ac67bbcd5a9d457
William Han <whan@godaddy.com>
2017-06-08 17:39:08 -0700

    add junit testng

---
+++ test/06-data-driven/junit-testng/src/test/java/com/company/app/TestngTest.java
@@ -0,0 +1,24 @@
+package com.company.app;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class TestngTest {
+  @BeforeClass
+  public void beforeClass(){
+    System.out.println("Before class executed");
+  }
+
+  @DataProvider
+  public Object[][] dataMethod(){
+    return new String[][]{
+        new String[]{"one"}, new String[]{"two"}
+    };
+  }
+
+  @Test(dataProvider = "dataMethod")
+  public void testMethod(String param){
+    System.out.println("The parameter value is: " + param);
+  }
+}
