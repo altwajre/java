324bce1fab88a9a4b90d49bc16f0aced4b945b9f
William Han <whan@godaddy.com>
2017-06-08 21:03:22 -0700

    update objectmapper

--- 11-maven-repository/jackson-databind/01-objectmapper/src/main/java/com/company/app/App.java
+++ 11-maven-repository/jackson-databind/01-objectmapper/src/main/java/com/company/app/App.java
@@ -2,31 +2,17 @@ package com.company.app;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import lombok.Data;
 
-class Student{
+@Data
+class Person {
     private String name;
     private int age;
+}
 
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString(){
-        return "Student [ name:: " + name + ", age: " + age + "]";
-    }
+@Data
+class Student extends Person{
+    private String arpartment;
 }
 
 public class App
