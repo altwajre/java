634ff13bf75da00c5fc275126f5357133da9de8d
William Han <whan@godaddy.com>
2017-06-21 18:03:38 -0700

    add setup teardown keyspace table

---
+++ test/11-frameworks/01-junit/21-databases/01-cassandra/01-setup-teardown-keyspace-table/src/test/java/com/company/app/DBHelper.java
@@ -0,0 +1,84 @@
+package com.company.app;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+
+public class DBHelper {
+
+  static Cluster cluster = Cluster.builder().addContactPoint("127.0.0.1")
+      .build();
+
+  // create session
+  static Session session = cluster.connect();
+
+  public static void createKeyspace() {
+
+    System.out.println("createKeyspace");
+
+    String query = "CREATE KEYSPACE IF NOT EXISTS home_security WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};";
+
+    session.execute(query);
+  }
+
+  public static void dropKeyspace() {
+
+    System.out.println("dropKeyspace");
+
+    String query = "DROP KEYSPACE IF EXISTS home_security;";
+
+    session.execute(query);
+  }
+
+  public static void connectKeyspace() {
+
+    System.out.println("connectKeyspace");
+
+    // Query
+    String query = "USE home_security";
+
+    // Executing the query
+    session.execute(query);
+  }
+
+  public static void createTables() {
+
+    System.out.println("createTables");
+
+    // Query
+    String query = "CREATE TABLE IF NOT EXISTS activity (home_id text, "
+        + "datetime timestamp, "
+        + "event text, "
+        + "code_used text, "
+        + "PRIMARY KEY (home_id, datetime));";
+
+    // Executing the query
+    session.execute(query);
+
+    query = "CREATE TABLE home (home_id text, address text, city text, state text, zip text, contact_name text, phone text, alt_phone text, phone_password text, email text, main_code text, guest_code text, PRIMARY KEY (home_id));";
+
+    // Executing the query
+    session.execute(query);
+  }
+
+  public static void dropTables() {
+
+    System.out.println("dropTables");
+
+    // Query
+    String query = "DROP TABLE IF EXISTS activity;";
+
+    query += "DROP TABLE IF EXISTS home;";
+
+    // Executing the query
+    session.execute(query);
+  }
+
+  public static void tearDown() {
+
+    System.out.println("tearDown");
+
+    session.close();
+    cluster.close();
+  }
+
+}
---
+++ test/11-frameworks/01-junit/21-databases/01-cassandra/01-setup-teardown-keyspace-table/src/test/java/com/company/app/SetupTeardownKeyspaceTableTest.java
@@ -0,0 +1,94 @@
+package com.company.app;
+
+import org.junit.*;
+
+/*
+CQL:
+-----------------------
+DESCRIBE KEYSPACES;
+
+CREATE KEYSPACE IF NOT EXISTS home_security WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};
+
+DESCRIBE KEYSPACE home_security;
+
+DROP KEYSPACE IF EXISTS home_security;
+
+-----------------------
+USE home_security;
+
+CREATE TABLE IF NOT EXISTS activity (
+home_id text,
+datetime timestamp,
+event text,
+code_used text,
+PRIMARY KEY (home_id, datetime)
+);
+
+DESCRIBE TABLE activity;
+
+DROP TABLE IF EXISTS activity;
+
+DESCRIBE TABLES;
+
+ */
+
+public class SetupTeardownKeyspaceTableTest {
+
+  @BeforeClass
+  public static void setUpClass() {
+
+    System.out.println("@BeforeClass setupClass");
+
+    DBHelper.createKeyspace();
+
+    DBHelper.connectKeyspace();
+
+    DBHelper.createTables();
+  }
+
+  @AfterClass
+  public static void tearDownClass() {
+
+    System.out.println("@AfterClass tearDownClass");
+
+//    DBHelper.dropTables();
+//
+//    DBHelper.dropKeyspace();
+
+    DBHelper.tearDown();
+  }
+
+  @Before
+  public void setUp() {
+    System.out.println("@Before setup");
+  }
+
+  @After
+  public void tearDown() {
+    System.out.println("@After tearDown");
+  }
+
+  @Test
+  public void test1() {
+    System.out.println("@Test test1()");
+  }
+
+  @Test
+  public void test2() {
+    System.out.println("@Test test2()");
+  }
+
+}
+/*
+output:
+createTables
+@Before setup
+@Test test1()
+@After tearDown
+@Before setup
+@Test test2()
+@After tearDown
+@AfterClass tearDownClass
+dropTables
+tearDown
+ */
