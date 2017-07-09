470f4ab490a841af71ba3a69d1eb1b028cbdb37b
William Han <whan@godaddy.com>
2017-07-09 16:30:18 -0700

    cleanup

--- 77-data/02-json/01-json-to-map-resources/src/main/java/com/company/app/App.java
+++ 77-data/02-json/01-json-to-map-resources/src/main/java/com/company/app/App.java
@@ -1,6 +1,8 @@
 package com.company.app;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.json.simple.JSONObject;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -42,20 +44,29 @@ output:
 
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         InputStream inputStream = classLoader.getResourceAsStream("json//test.json");
+
+        System.out.println("##Json to Map");
         Map mapJson = new ObjectMapper().readValue(inputStream, Map.class);
         System.out.println(mapJson);
 
-        Map classJson = (Map)mapJson.get("class");
-        System.out.println(classJson);
+        Map classMap = (Map)mapJson.get("class");
+        System.out.println(classMap);
 
-        Map studentsJson = (Map)classJson.get("students");
-        System.out.println(studentsJson);
+        Map studentsMap = (Map)classMap.get("students");
+        System.out.println(studentsMap);
+
+        System.out.println("##Map to Json");
+        JSONObject jsonObject = new JSONObject(classMap);
+        System.out.println(jsonObject);
     }
 /*
 output:
 #classLoader_test
+##Json to Map
 {class={name=Math, students={123=Tom, 234=Jen}}}
 {name=Math, students={123=Tom, 234=Jen}}
 {123=Tom, 234=Jen}
+##Map to Json
+{"name":"Math","students":{"123":"Tom","234":"Jen"}}
  */
 }
