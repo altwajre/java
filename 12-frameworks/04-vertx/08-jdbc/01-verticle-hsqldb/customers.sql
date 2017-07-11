b37ac949dc63a250d7867bd6f500d7f3ea8bd6a1
William Han <whan@godaddy.com>
2017-07-10 21:33:02 -0700

    add docker cassandra

---
+++ 12-frameworks/04-vertx/08-jdbc/01-verticle-hsqldb/customers.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS Customers
+CREATE TABLE IF NOT EXISTS Customers (name VARCHAR (20), age INT);
+INSERT INTO Customers (name,age) VALUES ('Tom', 28 );
+SELECT * FROM Customers;
--- 12-frameworks/04-vertx/08-jdbc/01-verticle-hsqldb/src/main/java/com/company/app/App.java
+++ 12-frameworks/04-vertx/08-jdbc/01-verticle-hsqldb/src/main/java/com/company/app/App.java
@@ -2,14 +2,6 @@ package com.company.app;
 
 import java.sql.*;
 
-/*
-SQL:
-DROP TABLE IF EXISTS Customers
-CREATE TABLE IF NOT EXISTS Customers (name VARCHAR (20), age INT);
-INSERT INTO Customers (name,age) VALUES ('Tom', 28 );
-SELECT * FROM Customers;
-
- */
 public class App
 {
     public static void main( String[] args ) throws Exception {
