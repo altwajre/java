5627c2e2e7496ab6be36f333ef902e05140c4232
William Han <whan@godaddy.com>
2017-07-21 22:29:41 -0700

    add jsonobject

---
+++ 12-frameworks/04-vertx/03-docs/02-vertx-core/06-json/01-jsonobject/src/main/java/com/company/app/App.java
@@ -0,0 +1,98 @@
+package com.company.app;
+
+import io.vertx.core.json.JsonObject;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+class User {
+  public User() {
+  }
+
+  private String name;
+  private Integer age;
+}
+
+public class App {
+  public static void main(String[] args) {
+    createFromString();
+
+    createFromMap();
+
+    putAndGet();
+
+    mapFromPojo();
+
+    mapToPojo();
+
+  }
+
+  private static void mapFromPojo() {
+    System.out.println("#mapFromPojo");
+    User user = new User("Tom", 38);
+    JsonObject jsonObject = JsonObject.mapFrom(user);
+    System.out.println(jsonObject);
+    System.out.println(jsonObject.getString("name"));
+    System.out.println(jsonObject.getInteger("age"));
+  }
+
+  private static void mapToPojo() {
+    System.out.println("#mapToPojo");
+    JsonObject jsonObject = new JsonObject();
+    jsonObject.put("name", "Tom").put("age", 28);
+    User user = jsonObject.mapTo(User.class);
+    System.out.println(user);
+  }
+
+  private static void putAndGet() {
+    System.out.println("#putAndGet");
+    JsonObject jsonObject = new JsonObject();
+    jsonObject.put("name", "Tom").put("age", 28);
+    System.out.println(jsonObject);
+    System.out.println(jsonObject.getString("name"));
+    System.out.println(jsonObject.getInteger("age"));
+  }
+
+  private static void createFromMap() {
+    System.out.println("#createFromMap");
+    Map<String, Object> map = new HashMap<>();
+    map.put("name", "Tom");
+    map.put("age", 18);
+    JsonObject jsonObject = new JsonObject(map);
+    System.out.println(jsonObject);
+    System.out.println(jsonObject.getString("name"));
+    System.out.println(jsonObject.getInteger("age"));
+  }
+
+  private static void createFromString() {
+    System.out.println("#createFromString");
+    String jsonString = "{\"name\":\"Tom\"}";
+    JsonObject jsonObject = new JsonObject(jsonString);
+    System.out.println(jsonObject);
+    System.out.println(jsonObject.getString("name"));
+  }
+}
+/*
+output:
+#createFromString
+{"name":"Tom"}
+Tom
+#createFromMap
+{"name":"Tom","age":18}
+Tom
+18
+#putAndGet
+{"name":"Tom","age":28}
+Tom
+28
+#mapFromPojo
+{"name":"Tom","age":38}
+Tom
+38
+#mapToPojo
+User(name=Tom, age=28)
+ */
