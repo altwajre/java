79f9295e91bfa39b8af9acf0c6f705de25b19063
William Han <whan@godaddy.com>
2017-08-03 15:48:50 -0700

    add asserting

---
+++ 12-frameworks/04-vertx/03-docs/12-testing/03-asserting/src/test/java/com/company/app/AppTest.java
@@ -0,0 +1,76 @@
+package com.company.app;
+
+import io.vertx.ext.unit.TestOptions;
+import io.vertx.ext.unit.TestSuite;
+import io.vertx.ext.unit.report.ReportOptions;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AppTest {
+  @Test
+  public void runTest() {
+
+//    Assert.assertEquals("", "");
+    TestSuite suite = TestSuite.create("the_test_suite");
+
+    suite.test("assertEquals", testContext -> {
+      String expected = "1";
+      String actual = "1";
+      testContext.assertEquals(expected, actual, "Should have been " + expected + " instead of " + actual);
+    });
+
+    suite.test("assertNotEquals", testContext -> {
+      String expected = "1";
+      String actual = "2";
+      testContext.assertNotEquals(expected, actual);
+    });
+
+    suite.test("assertNull", testContext -> {
+      String expected = null;
+      testContext.assertNull(expected);
+    });
+
+    suite.test("assertNull", testContext -> {
+      String expected = "1";
+      testContext.assertNotNull(expected);
+    });
+
+    suite.test("assertTrue", testContext -> {
+      Boolean expected = true;
+      testContext.assertTrue(expected);
+    });
+
+    suite.test("assertFalse", testContext -> {
+      Boolean expected = false;
+      testContext.assertFalse(expected);
+    });
+
+    suite.test("fail", testContext -> {
+      testContext.fail("That should never happen");
+    });
+
+    suite.run(new TestOptions().addReporter(new ReportOptions().setTo("console")));
+  }
+}
+/*
+Begin test suite the_test_suite
+Begin test assertEquals
+Passed assertEquals
+Begin test assertNotEquals
+Passed assertNotEquals
+Begin test assertNull
+Passed assertNull
+Begin test assertNull
+Passed assertNull
+Begin test assertTrue
+Passed assertTrue
+Begin test assertFalse
+Passed assertFalse
+Begin test fail
+Failed fail
+java.lang.AssertionError: That should never happen
+	at io.vertx.ext.unit.impl.TestContextImpl.reportAssertionError(TestContextImpl.java:432)
+	at io.vertx.ext.unit.impl.TestContextImpl.fail(TestContextImpl.java:310)
+	at com.company.app.AppTest.lambda$runTest$6(AppTest.java:50)
+ */
