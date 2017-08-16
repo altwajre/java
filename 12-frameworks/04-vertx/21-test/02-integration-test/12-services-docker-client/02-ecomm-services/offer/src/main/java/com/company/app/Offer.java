c496327a4ce0e5ba55c10103f5268919783502b7
William Han <whan@godaddy.com>
2017-08-16 15:27:34 -0700

    add NoArgsConstructor

--- 12-frameworks/04-vertx/21-test/02-integration-test/12-services-docker-client/02-ecomm-services/offer/src/main/java/com/company/app/Offer.java
+++ 12-frameworks/04-vertx/21-test/02-integration-test/12-services-docker-client/02-ecomm-services/offer/src/main/java/com/company/app/Offer.java
@@ -1,15 +1,15 @@
 package com.company.app;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class Offer {
   private int id;
   private String name;
   private Product product;
-  public Offer(){
 
-  }
   public Offer(int id, String name) {
     this.id = id;
     this.name = name;
