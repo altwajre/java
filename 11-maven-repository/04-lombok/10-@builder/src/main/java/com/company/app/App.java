42dde1e107b94984a666369d90dd91e782b77db6
William Han <whan@godaddy.com>
2017-08-16 22:38:09 -0700

    add maven builder

--- 11-maven-repository/04-lombok/10-@builder/src/main/java/com/company/app/App.java
+++ 11-maven-repository/04-lombok/10-@builder/src/main/java/com/company/app/App.java
@@ -1,25 +1,9 @@
 package com.company.app;
 
-import lombok.Builder;
-import lombok.Singular;
-import lombok.Value;
+import lombok.*;
 
 import java.util.Set;
 
-@Value
-@Builder
-class BuilderExample{
-    private String name;
-    private int age;
-    @Singular
-    private Set<String> occupations;
-}
-public class App {
-    public static void main(String... args){
-        BuilderExample builderExample = BuilderExample.builder().name("Tom").age(10).build();
-        System.out.println(builderExample);
-    }
-}
 /*
 https://projectlombok.org/features/Builder.html
 
@@ -27,8 +11,51 @@ The @Builder annotation produces complex builder APIs for your class.
 @Builder lets you automatically produce the code required to have your class be instantiable with code such as:
 Person.builder().name(“Tom”).age(8).build();
 
-The @Singular annotation treats that
+The @Singular annotation, lombok will treat that builder node as a collection, and it generates 2 'adder' methods
+instead of a 'setter' method.
+ */
+@Value
+@Builder
+class BuilderExample {
+  private String name;
+  private int age;
+  @Singular
+  private Set<String> occupations;
+}
+@Data
+class Customer {
+  private String name;
+  private int age;
+  public Customer() {
+    System.out.println("default constructor");
+  }
+  @Builder
+  public Customer(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+public class App {
+  public static void main(String[] args) {
+    BuilderExample builderExample = BuilderExample.builder().name("Tom").age(10).build();
+    System.out.println(builderExample);
 
-output:
+    final BuilderExample builderExample1 = new BuilderExample("Dick", 28, null);
+    System.out.println(builderExample1);
+
+    // Customer.builder() calls @Builder ctor
+    final Customer tom = Customer.builder().name("Tom").age(18).build();
+    System.out.println(tom);
+
+    // calls default ctor
+    final Customer customer = new Customer();
+    System.out.println(customer);
+  }
+}
+/*
 BuilderExample(name=Tom, age=10, occupations=[])
+BuilderExample(name=Dick, age=28, occupations=null)
+Customer(name=Tom, age=18)
+default constructor
+Customer(name=null, age=0)
  */
