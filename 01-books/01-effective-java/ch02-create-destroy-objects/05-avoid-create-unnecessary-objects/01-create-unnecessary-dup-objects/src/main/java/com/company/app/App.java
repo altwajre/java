ee6daa63e5cc9cd944633babba1a61d090249ef5
William Han <whan@godaddy.com>
2017-08-23 16:10:00 -0700

    update avoid creating unnecessary objects

--- 01-books/01-effective-java/ch02-create-destroy-objects/05-avoid-create-unnecessary-objects/01-create-unnecessary-dup-objects/src/main/java/com/company/app/App.java
+++ 01-books/01-effective-java/ch02-create-destroy-objects/05-avoid-create-unnecessary-objects/01-create-unnecessary-dup-objects/src/main/java/com/company/app/App.java
@@ -1,40 +1,66 @@
 package com.company.app;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.TimeZone;
+import java.util.*;
 
 // Create lots of unnecessary duplicate objects
-class Person{
-    private final Date birthDate;
-    public Person(Date birthDate){
-        // Defensive copy
-        this.birthDate = new Date(birthDate.getTime());
-    }
-    // Other fields, methods omitted
-    // DON"T DO THIS!
-    public boolean isBabyBoomer(){
-        // Unnecessary allocation of expensive object
-        Calendar gmtCal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
-        gmtCal.set(1946, Calendar.JANUARY, 1, 0, 0, 0);
-        Date boomStart = gmtCal.getTime();
-        gmtCal.set(1965, Calendar.JANUARY, 1, 0, 0, 0);
-        Date boomEnd = gmtCal.getTime();
-        return birthDate.compareTo(boomStart) >= 0 && birthDate.compareTo(boomEnd) < 0;
-    }
+class Person {
+  private final Date birthDate;
+
+  public Person(Date birthDate) {
+    // Defensive copy
+    this.birthDate = new Date(birthDate.getTime());
+  }
+
+  // Other fields, methods omitted
+  // DON"T DO THIS!
+  public boolean isBabyBoomer() {
+    System.out.println("instance: get start_date and end_date");
+    // Unnecessary allocation of expensive object
+    Calendar gmtCal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+    gmtCal.set(1946, Calendar.JANUARY, 1, 0, 0, 0);
+    Date boomStart = gmtCal.getTime();
+    gmtCal.set(1965, Calendar.JANUARY, 1, 0, 0, 0);
+    Date boomEnd = gmtCal.getTime();
+    return birthDate.compareTo(boomStart) >= 0 && birthDate.compareTo(boomEnd) < 0;
+  }
 }
-public class App
-{
-    public static void main( String[] args ) throws Exception {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
-        String dateInString = "31-08-1982 10:20:56";
-        Person tom = new Person(sdf.parse(dateInString));
-        System.out.println("Tom isBabyBoomer=" + tom.isBabyBoomer());
+
+public class App {
+  public static void main(String[] args) throws Exception {
+    SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
+    String dateInString = "31-08-1982 10:20:56";
+    Person tom = new Person(sdf.parse(dateInString));
+    System.out.println("Tom isBabyBoomer=" + tom.isBabyBoomer());
+//    System.in.read();
+    // Creating lots of Person objects and keep them alive
+    List<Person> people = new ArrayList<>();
+    for(int i = 0; i< 10; i++){
+      final Person person = new Person(sdf.parse(dateInString));
+      people.add(person);
+      person.isBabyBoomer();
     }
+//    System.in.read();
+    System.out.println(people.size());
+    System.out.println("END");
+  }
 }
 /*
-output:
+instance: get start_date and end_date
 Tom isBabyBoomer=false
+instance: get start_date and end_date
+instance: get start_date and end_date
+instance: get start_date and end_date
+instance: get start_date and end_date
+instance: get start_date and end_date
+instance: get start_date and end_date
+instance: get start_date and end_date
+instance: get start_date and end_date
+instance: get start_date and end_date
+instance: get start_date and end_date
+10
+END
+
+ISSUE:
+DUP objects - "instance: get start_date and end_date" is called each time when person.isBabyBoomer() is invoked
  */
--- 01-books/01-effective-java/ch02-create-destroy-objects/05-avoid-create-unnecessary-objects/02-no-unnecessary-dup-objects/src/main/java/com/company/app/App.java
+++ 01-books/01-effective-java/ch02-create-destroy-objects/05-avoid-create-unnecessary-objects/02-no-unnecessary-dup-objects/src/main/java/com/company/app/App.java
@@ -1,43 +1,58 @@
 package com.company.app;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.TimeZone;
+import java.util.*;
 
 // Doesn't create unnecessary duplicate objects
-class Person{
-    private final Date birthDate;
-    public Person(Date birthDate) {
-        // Defensive copy
-        this.birthDate = new Date(birthDate.getTime());
-    }
-    // Other fields, methods
-    // The starting and ending dates of the baby boom.
-    private static final Date BOOM_START;
-    private static final Date BOOM_END;
-    static {
-        Calendar gmtCal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
-        gmtCal.set(1946, Calendar.JANUARY, 1, 0, 0, 0);
-        BOOM_START = gmtCal.getTime();
-        gmtCal.set(1965, Calendar.JANUARY, 1, 0, 0, 0);
-        BOOM_END = gmtCal.getTime();
-    }
-    public boolean isBabyBoomer(){
-        return birthDate.compareTo(BOOM_START) >= 0 && birthDate.compareTo(BOOM_END) < 0;
-    }
+class Person {
+  private final Date birthDate;
+
+  public Person(Date birthDate) {
+    // Defensive copy
+    this.birthDate = new Date(birthDate.getTime());
+  }
+
+  // Other fields, methods
+  // The starting and ending dates of the baby boom.
+  private static final Date BOOM_START;
+  private static final Date BOOM_END;
+
+  static {
+    System.out.println("static: get start_date and end_date");
+    Calendar gmtCal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+    gmtCal.set(1946, Calendar.JANUARY, 1, 0, 0, 0);
+    BOOM_START = gmtCal.getTime();
+    gmtCal.set(1965, Calendar.JANUARY, 1, 0, 0, 0);
+    BOOM_END = gmtCal.getTime();
+  }
+
+  public boolean isBabyBoomer() {
+    return birthDate.compareTo(BOOM_START) >= 0 && birthDate.compareTo(BOOM_END) < 0;
+  }
 }
-public class App
-{
-    public static void main( String[] args ) throws Exception {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
-        String dateInString = "31-08-1982 10:20:56";
-        Person tom = new Person(sdf.parse(dateInString));
-        System.out.println("Tom isBabyBoomer=" + tom.isBabyBoomer());
+
+public class App {
+  public static void main(String[] args) throws Exception {
+    SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
+    String dateInString = "31-08-1982 10:20:56";
+    Person tom = new Person(sdf.parse(dateInString));
+    System.out.println("Tom isBabyBoomer=" + tom.isBabyBoomer());
+//    System.in.read();
+    // Creating lots of Person objects and keep them alive
+    List<Person> people = new ArrayList<>();
+    for (int i = 0; i < 10; i++) {
+      final Person person = new Person(sdf.parse(dateInString));
+      people.add(person);
+      person.isBabyBoomer();
     }
+//    System.in.read();
+    System.out.println(people.size());
+    System.out.println("END");
+  }
 }
 /*
-output:
+static: get start_date and end_date
 Tom isBabyBoomer=false
+10
+END
  */
