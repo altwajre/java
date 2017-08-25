9fde22d046c3759a212133666d3cbec3a3ef5d6b
William Han <whan@godaddy.com>
2017-08-25 14:25:32 -0700

    add building first junit test

---
+++ test/01-books/01-java-8-with-junit/01-building-first-junit-test/src/test/java/com/company/app/ScoreCollectionTest.java
@@ -0,0 +1,46 @@
+package com.company.app;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.*;
+
+public class ScoreCollectionTest {
+  @Test
+  public void answersArithmeticMeanOfTwoNumbers() {
+    // Arrange
+    ScoreCollection collection = new ScoreCollection();
+    collection.add(() -> 5);
+    collection.add(() -> 7);
+
+    // Act
+    int actualResult = collection.arithmeticMean();
+
+    // Assert
+    assertThat(actualResult, equalTo(6));
+  }
+
+  @Test
+  public void testFunctionalInterfaceLambda() {
+    final Scoreable scoreable = () -> 8;
+    System.out.println(scoreable.getScore());
+  }
+/*
+8
+ */
+
+  @Test
+  public void testFunctionalInterfaceAnonymousClass() {
+    final Scoreable scoreable = new Scoreable() {
+      @Override
+      public int getScore() {
+        return 8;
+      }
+    };
+    System.out.println(scoreable.getScore());
+  }
+/*
+8
+ */
+
+}
