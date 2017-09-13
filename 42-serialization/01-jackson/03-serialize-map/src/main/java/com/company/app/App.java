a308757cd3b63f0a8a1d21c7b8c27720a568b685
William Han <whan@godaddy.com>
2017-09-13 11:54:27 -0700

    rename

---
+++ 42-serialization/01-jackson/03-serialize-map/src/main/java/com/company/app/App.java
@@ -0,0 +1,10 @@
+package com.company.app;
+
+/*
+http://www.baeldung.com/jackson-map
+ */
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
