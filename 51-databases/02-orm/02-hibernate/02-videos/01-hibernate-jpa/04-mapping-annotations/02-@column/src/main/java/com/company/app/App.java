9fa0a655c4f3022c972a87b00b4719692c3619f9
William Han <whan@godaddy.com>
2017-09-24 16:07:18 -0700

    rename

--- 51-databases/02-orm/02-hibernate/02-videos/01-hibernate-jpa/04-mapping-annotations/02-@column/src/main/java/com/company/app/App.java
+++ 51-databases/02-orm/02-hibernate/02-videos/01-hibernate-jpa/04-mapping-annotations/02-@column/src/main/java/com/company/app/App.java
@@ -11,6 +11,8 @@ import java.util.Date;
 
 /*
 
+https://www.safaribooksonline.com/library/view/hibernate-and-java/9781771373494/video209925.html
+
 > required field - nullable = false
   @Column(name = "BIRTH_DATE", nullable = false)
   private Date birthDate;
@@ -18,6 +20,57 @@ import java.util.Date;
 > no update field - updatable = false
   @Column(name = "CREATED_DATE", updatable = false)
   private Date createdDate;
+
+-----------------------------------------------------
+
+1. resources/hibernate.cfg.xml - it is loaded by .applySettings(configuration.getProperties())
+<hibernate-configuration>
+  <session-factory>
+    <!-- Database connection settings -->
+    <property name="connection.driver_class">com.mysql.cj.jdbc.Driver</property>
+    <property name="connection.url">jdbc:mysql://localhost:3306/ifinances</property>
+    <property name="connection.username">infinite</property>
+    <property name="connection.password">skills</property>
+    <!-- SQL dialect -->
+    <property name="dialect">org.hibernate.dialect.MySQL5Dialect</property>
+    <!-- Echo all executed SQL to stdout -->
+    <property name="show_sql">true</property>
+  </session-factory>
+</hibernate-configuration>
+
+2, add User to config and load xml config
+    Configuration configuration = new Configuration();
+    configuration.addAnnotatedClass(User.class);
+    configuration.configure();
+    return configuration
+        .buildSessionFactory(new StandardServiceRegistryBuilder()
+            .applySettings(configuration.getProperties()) // load xml config
+            .build());
+
+3, run InfiniteFinancesSchema.sql first before running this app
+
+4, database - finances_user table
+
+SET FOREIGN_KEY_CHECKS=0;
+DROP TABLE IF EXISTS `finances_user`;
+SET FOREIGN_KEY_CHECKS=1;
+CREATE TABLE `finances_user` (
+    `USER_ID` bigint(20) NOT NULL AUTO_INCREMENT,
+    `FIRST_NAME` varchar(45) NOT NULL,
+    `LAST_NAME` varchar(45) NOT NULL,
+    `BIRTH_DATE` date NOT NULL,
+    `EMAIL_ADDRESS` varchar(100) NOT NULL,
+    `LAST_UPDATED_BY` varchar(45) NOT NULL,
+    `LAST_UPDATED_DATE` datetime NOT NULL,
+    `CREATED_BY` varchar(45) NOT NULL,
+    `CREATED_DATE` datetime NOT NULL,
+    `USER_ADDRESS_LINE_1` varchar(100) DEFAULT NULL,
+    `USER_ADDRESS_LINE_2` varchar(100) DEFAULT NULL,
+    `CITY` varchar(100) DEFAULT NULL,
+    `STATE` varchar(2) DEFAULT NULL,
+    `ZIP_CODE` varchar(5) DEFAULT NULL,
+    PRIMARY KEY (`USER_ID`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
  */
 public class App {
@@ -42,3 +95,6 @@ public class App {
     sessionFactory.close();
   }
 }
+/*
+Hibernate: insert into finances_user (BIRTH_DATE, CREATED_BY, CREATED_DATE, EMAIL_ADDRESS, FIRST_NAME, LAST_NAME, LAST_UPDATED_BY, LAST_UPDATED_DATE) values (?, ?, ?, ?, ?, ?, ?, ?)
+ */
