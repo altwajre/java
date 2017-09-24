591fbec9d5efc21430832e93475de8a47eec3dbf
William Han <whan@godaddy.com>
2017-09-24 16:29:35 -0700

    add compare strings

--- 04-essential/01-compare-two-objects/src/main/java/com/company/app/App.java
+++ 04-essential/01-compare-two-objects/src/main/java/com/company/app/App.java
@@ -13,33 +13,21 @@ class User {
 /*
 
 1, == operator compare two objects are the same instance
-2, .equal() method compare two objects are equivalent
-
+2, .equals() method compare two objects are equivalent
+3, compare strings should use .equals() because we are comparing the string values
  */
 public class App {
   public static void main(String[] args) {
     equalOperator();
 
-//    equalMethod();
-  }
+    equalMethod();
 
-  // .equal() method compare two objects are equivalent
-  private static void equalMethod() {
-    User user1 = new User("Tom", 18);
-    User user2 = new User("Tom", 18);
-    if(user1.equals(user2)) {
-      System.out.println("PASS: user1 and user2 have the same field values (equivalent)");
-    }
-    else {
-      System.err.println("PASS: user1 and user2 DO NOT have the same field values (NOT equivalent)");
-    }
+    compareStrings();
   }
-/*
-PASS: user1 and user2 have the same field values (equivalent)
- */
 
   // == operator compare two objects are the same instance
   private static void equalOperator() {
+    System.out.println("#equalOperator");
     User user1 = new User("Tom", 18);
     User user2 = user1;
 
@@ -52,5 +40,38 @@ PASS: user1 and user2 have the same field values (equivalent)
   }
 /*
 PASS: user1 and user2 reference the same object
+ */
+
+  // .equal() method compare two objects are equivalent
+  private static void equalMethod() {
+    System.out.println("#equalMethod");
+    User user1 = new User("Tom", 18);
+    User user2 = new User("Tom", 18);
+    if(user1.equals(user2)) {
+      System.out.println("PASS: user1 and user2 have the same field values (equivalent)");
+    }
+    else {
+      System.err.println("FAIL: user1 and user2 DO NOT have the same field values (NOT equivalent)");
+    }
+  }
+/*
+PASS: user1 and user2 have the same field values (equivalent)
+ */
+
+  // compare strings should use .equals() because we are comparing the string values
+  private static void compareStrings() {
+    System.out.println("#compareStrings");
+    String name1 = "Tom";
+    String name2 = "Tom";
+
+    if(name1.equals(name2)) {
+      System.out.println("PASS: name1 and name2 have the same values (equivalent)");
+    }
+    else {
+      System.out.println("FAIL: name1 and name2 DO NOT have the same values (equivalent)");
+    }
+  }
+/*
+PASS: name1 and name2 have the same values (equivalent)
  */
 }
