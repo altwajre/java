f00889e6a156cf17d6d9f9e8614ac14d6bbd54a3
William Han <pwillhan@gmail.com>
2017-10-10 18:34:55 -0700

    update

--- 42-serialization/01-jackson/01-objectmapper-json/src/main/java/com/company/app/App.java
+++ 42-serialization/01-jackson/01-objectmapper-json/src/main/java/com/company/app/App.java
@@ -1,41 +1,59 @@
 package com.company.app;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import lombok.Data;
 
 @Data
 class Person {
-    private String name;
-    private int age;
+  @JsonProperty
+  private String name;
+  @JsonProperty
+  private int age;
 }
 
 @Data
-class Student extends Person{
-    private String arpartment;
+class Student extends Person {
+  @JsonProperty
+  private String arpartment;
 }
 
 /*
 http://www.tutorialspoint.com/jackson/jackson_objectmapper.htm
  */
-public class App
-{
-    public static void main( String[] args ) throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
-        String jsonString = "{\"name\":\"Mahesh\", \"age\":21}";
+public class App {
+  public static void main(String[] args) throws Exception {
+    ObjectMapper mapper = new ObjectMapper();
+    String jsonString = "{\"name\":\"Mahesh\", \"age\":21, \"arpartment\":\"128\"}";
 
-        Student student = mapper.readValue(jsonString, Student.class);
-        System.out.println(student);
+    Student student = deserialize(mapper, jsonString);
 
-        mapper.enable(SerializationFeature.INDENT_OUTPUT);
-        String jsonStr = mapper.writeValueAsString(student);
-        System.out.println(jsonStr);
-    }
+    serialize(mapper, student);
+  }
+
+  private static void serialize(ObjectMapper mapper, Student student) throws JsonProcessingException {
+    System.out.println("#serialize json-string");
+    mapper.enable(SerializationFeature.INDENT_OUTPUT);
+    String jsonStr = mapper.writeValueAsString(student);
+    System.out.println(jsonStr);
+  }
+
+  private static Student deserialize(ObjectMapper mapper, String jsonString) throws java.io.IOException {
+    System.out.println("#deserialize");
+    Student student = mapper.readValue(jsonString, Student.class);
+    System.out.println(student);
+    return student;
+  }
 }
 /*
-Student [ name:: Mahesh, age: 21]
+#deserialize
+Student(arpartment=128)
+#serialize json-string
 {
   "name" : "Mahesh",
-  "age" : 21
+  "age" : 21,
+  "arpartment" : "128"
 }
  */
