fd642bf9c92631debadb50ab4375171cead94cbd
William Han <pwillhan@gmail.com>
2017-10-18 17:11:56 -0700

    update

--- 51-databases/22-orm/02-hibernate/05-persistence-layer/02-jpa-dao/src/main/java/com/company/app/App.java
+++ 51-databases/22-orm/02-hibernate/05-persistence-layer/02-jpa-dao/src/main/java/com/company/app/App.java
@@ -3,11 +3,15 @@ package com.company.app;
 import com.company.app.dao.UserJpaDao;
 import com.company.app.dao.interfaces.UserDao;
 import com.company.app.model.User;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 import java.util.Date;
+import java.util.List;
 
 /*
 https://www.safaribooksonline.com/library/view/hibernate-and-java/9781771373494/video209973.html
@@ -63,6 +67,7 @@ it covers CRUD
 
  */
 public class App {
+  private static ObjectMapper MAPPER = new ObjectMapper();
   public static void main(String[] args) {
 
     EntityManagerFactory entityManagerFactory = null;
@@ -90,12 +95,29 @@ public class App {
 //      delete(userDao, updatedUser);
 
       entityManager.getTransaction().commit();
+
+      nativeQuery(entityManager);
+
     } catch (Exception e) {
       entityManager.getTransaction().rollback();
     } finally {
       entityManager.close();
       entityManagerFactory.close();
     }
+  }
+
+  private static void nativeQuery(EntityManager entityManager) {
+    entityManager.getTransaction().begin();
+    Query nativeQuery = entityManager.createNativeQuery("SELECT * FROM ifinances.finances_user");
+    List resultList = nativeQuery.getResultList();
+    resultList.forEach(u -> {
+      try {
+        System.out.println(MAPPER.writeValueAsString(u));
+      } catch (JsonProcessingException e) {
+        e.printStackTrace();
+      }
+    });
+    entityManager.getTransaction().commit();
   }
 
   private static void delete(UserDao userDao, User updatedUser) {
@@ -126,8 +148,10 @@ public class App {
 }
 /*
 Hibernate: insert into finances_user (BIRTH_DATE, CREATED_BY, CREATED_DATE, EMAIL_ADDRESS, FIRST_NAME, LAST_NAME, LAST_UPDATED_BY, LAST_UPDATED_DATE) values (?, ?, ?, ?, ?, ?, ?, ?)
-Create - UserId=14
-Get - User FirstName=Tom
-Update - UserId=14, FirstName=Tommy
-Hibernate: delete from finances_user where USER_ID=?
+Create - UserId=17
+Read - User FirstName=Tom
+Update - UserId=17, FirstName=Tommy
+Hibernate: update finances_user set BIRTH_DATE=?, EMAIL_ADDRESS=?, FIRST_NAME=?, LAST_NAME=?, LAST_UPDATED_BY=?, LAST_UPDATED_DATE=? where USER_ID=?
+Hibernate: SELECT * FROM ifinances.finances_user
+[1,"Tommy","Lee","2017-10-17","tom168@yahoo.com","tom",1508370532000,"tom",1508370532000,null,null,null,null,null]
  */
