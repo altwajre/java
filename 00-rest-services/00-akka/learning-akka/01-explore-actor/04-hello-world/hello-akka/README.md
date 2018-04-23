909ea958cc4ab94a4fb7a7e6cfa10ca4cfe7a0f8
William Han <whan@godaddy.com>
2018-04-23 15:47:14 -0700

    add akka

---
+++ 00-rest-services/00-akka/learning-akka/01-explore-actor/04-hello-world/hello-akka/README.md
@@ -0,0 +1,17 @@
+# Create Hello World
+
+## Run the app
+
+```
+$ sbt
+[info] Set current project to hello-akka (in build file:)
+> run
+[info] Compiling 1 Scala source to
+[info] Running HelloAkkaScala
+Hello Akka
+[success] Total time: 5 s, completed Dec 26, 2016 9:46:17 PM
+```
+
+- intellij
+
+Right click `object HelloAkka` body, and click `Run HelloAkka`
---
+++ 00-rest-services/00-akka/learning-akka/01-explore-actor/04-hello-world/hello-akka/src/main/scala/HelloAkka.scala
@@ -0,0 +1,36 @@
+import akka.actor.{ ActorRef, ActorSystem, Props, Actor }
+import scala.concurrent.duration._
+
+// Define Actor Messages
+case class WhoToGreet(who: String)
+
+// Define Greeter Actor
+class Greeter extends Actor {
+  def receive = {
+    case WhoToGreet(who) => println(s"Hello $who")
+  }
+}
+
+object HelloAkka extends App {
+
+  // Create the 'hello akka' actor system
+  val system = ActorSystem("Hello-Akka")
+
+  // Create the 'greeter' actor
+  val greeter = system.actorOf(Props[Greeter], "greeter")
+
+  // Send WhoToGreet Message to actor
+  greeter ! WhoToGreet("Akka")
+
+  //shutdown actorsystem
+  system.terminate()
+
+}
+
+//$ sbt
+//[info] Set current project to hello-akka (in build file:)
+//> run
+//[info] Compiling 1 Scala source to
+//[info] Running HelloAkkaScala
+//Hello Akka
+//[success] Total time: 5 s, completed Dec 26, 2016 9:46:17 PM
