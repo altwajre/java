2e2eef830dd02ad2a0864efc78cb2971c233ec4b
William Han <whan@godaddy.com>
2018-08-25 15:41:17 -0700

    rename

--- 01-akka/00-akka-cookbook/09-akka-http/src/main/scala/HttpServerRoutingDSL.scala
+++ 01-akka/00-akka-cookbook/09-akka-http/src/main/scala/HttpServerRoutingDSL.scala
@@ -1,3 +1,4 @@
+import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server._
 
@@ -21,13 +22,44 @@ trait InMemoryStorageRestApi[K, V] {
   }
 
   private def handleAllMethods(cache: mutable.Map[K, V]) = {
-    get { handleGet(cache)}
+    get {
+      handleGet(cache)
+    } ~
+      post {
+        handlePost(cache)
+      } ~
+      put {
+        handlePut(cache)
+      } ~
+      delete {
+        handleDelete(cache)
+      }
   }
 
   def handleGet(cache: mutable.Map[K, V]): Route
+
   def handlePut(cache: mutable.Map[K, V]): Route
+
   def handlePost(cache: mutable.Map[K, V]): Route
+
   def handleDelete(cache: mutable.Map[K, V]): Route
 }
 
+case class TemperatureMeasurement(location: String, measurement: Double)
+
+trait GetRequestsHandler {
+  def handleGet(cache: mutable.Map[String, TemperatureMeasurement]) = pathEndOrSingleSlash {
+    complete {
+      cache.map(keyValue => s"${keyValue._2.location}, ${keyValue._2.measurement}").mkString("n")
+    }
+  } ~
+  path(Segment) { id =>
+    complete {
+      cache.get(id) match {
+        case Some(TemperatureMeasurement(location, measurement)) => s"Temperature for $location is $measurement"
+        case None => StatusCodes.NotFound -> s"Not temperature measurement for $id"
+      }
+    }
+  }
+}
 
--- 01-scala/references.md
+++ 01-scala/references.md
@@ -1,5 +1,9 @@
 # References
 
+## Symbols
+
+https://docs.scala-lang.org/tutorials/FAQ/finding-symbols.html
+
 ## Underscore
 
 https://stackoverflow.com/questions/8000903/what-are-all-the-uses-of-an-underscore-in-scala
