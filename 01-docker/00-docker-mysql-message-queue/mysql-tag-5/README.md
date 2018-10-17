24188ad7d26b39076d0abf1d3e94057af577a55e
whan-gd <whan@godaddy.com>
2018-10-17 11:00:29 -0700

    add simple rabbitmq

--- 01-docker/00-docker-mysql-message-queue/mysql-tag-5/README.md
+++ 01-docker/00-docker-mysql-message-queue/mysql-tag-5/README.md
@@ -2,9 +2,48 @@
 
 https://www.rabbitmq.com/tutorials/tutorial-two-java.html
 
+> rabbitmq
+
+docker run -d --hostname myrabbit --name myrabbit -p 5672:5672 -p 15672:15672 rabbitmq:3-management
+
 > mysql
 
-- setup-mysql.md
+https://hub.docker.com/_/mysql/
+
+docker run --name mysql-5 \
+-e MYSQL_ROOT_PASSWORD=root \
+-e MYSQL_DATABASE=test \
+-p 3306:3306 \
+-d mysql:5.7.23
+
+docker stop <containerId>
+docker start <containerId>
+
+> login to docker container to check database
+
+docker exec -it mysql-5 sh
+
+mysql -u root -p
+Enter password: <- root
+
+mysql> show databases;
+create database ifinances;
+GRANT ALL PRIVILEGES ON ifinances.* TO 'infinite'@'%' IDENTIFIED BY 'skills';
+use ifinances;
+
+> Workbench
+User: infinite
+Password: skills
+
+run InfiniteFinancesSchema.sql in mysql workbench
+
+## cleanup
+
+- Force delete all containers
+docker rm -f $(docker ps -a -q)
+- Force delete all images
+docker rmi -f $(docker images -q)
+
 - run InfiniteFinancesSchema.sql in mysql workbench before running Apps
 
 > Run Apps in intellij
---
+++ 01-docker/00-docker-rabbitmq/receiver-simple/src/main/java/com/company/app/App.java
@@ -0,0 +1,34 @@
+package com.company.app;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+public class App {
+  private final static String QUEUE_NAME = "hello";
+
+  public static void main(String[] args) throws IOException {
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost("localhost");
+    Connection connection = factory.newConnection();
+    Channel channel = connection.createChannel();
+
+    // queueDeclare(String queue, boolean durable, boolean exclusive, boolean autoDelete, Map<String, Object> arguments)
+    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+    System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+    Consumer consumer = new DefaultConsumer(channel) {
+      @Override
+      public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws UnsupportedEncodingException {
+        String message = new String(body, "UTF-8");
+        System.out.println(" [x] Received '" + message + "'");
+      }
+    };
+    // basicConsume(String queue, boolean autoAck, Consumer callback)
+    channel.basicConsume(QUEUE_NAME, true, consumer);
+  }
+}
+/*
+ [*] Waiting for messages. To exit press CTRL+C
+ */
